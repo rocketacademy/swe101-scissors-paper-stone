31ecd89af5a1e438b6fc08abed97722300b35d72
Inna Abadilla <innaabadilla@Innas-MacBook-Air.local>
2020-08-23 22:16:35 +0800

    added draw-win-loss count feature

--- script.js
+++ script.js
@@ -7,10 +7,17 @@
 // 'paper' = 2
 // 'stone' = 3
 
+var drawNum = 0;
+var winNum = 0;
+var lossNum = 0;
+var invalidNum = 0;
+
 var main = function (input) {
   var numWeapon = randomWeapon(); // 1 to 3
   console.log(numWeapon); // Displays numWeapon in the console
   var nameWeapon;
+  invalidNum = invalidNum + 1;
+
 
   if (numWeapon == 1) {
     nameWeapon = 'scissors'
@@ -24,46 +31,55 @@ var main = function (input) {
     nameWeapon = 'stone'
   };
 
-  var myOutputValue = 'ha! nice try! ' + input + ' is not a valid weapon. >:(';
+  var myOutputValue = 'ha! nice try! ' + input + ' is a bogus weapon. >:( you have made ' + invalidNum + ' invalid attempt/s.';
 
   //DRAW
   if (input == 'scissors' && nameWeapon == 'scissors') {
-    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :)';
+    drawNum = drawNum + 1;
+    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (input == 'paper' && nameWeapon == 'paper') {
-    myOutputValue = ' Draw! Both of you selected paper ' + nameWeapon + ' :)';
+    drawNum = drawNum + 1;
+    myOutputValue = ' draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (input == 'stone' && nameWeapon == 'stone') {
-    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :)';
+    drawNum = drawNum + 1;
+    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
 
   //WIN
   if (input == 'scissors' && nameWeapon == 'paper') {
-    myOutputValue = 'hooray! ' + input + ' shreds ' + nameWeapon + '. you win!';
+    winNum = winNum + 1;
+    myOutputValue = 'hooray! ' + input + ' shreds ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (input == 'paper' && nameWeapon == 'stone') {
-    myOutputValue = 'hooray! ' + input + ' suffocates ' + nameWeapon + '. you win!';
+    winNum = winNum + 1;
+    myOutputValue = 'hooray! ' + input + ' suffocates ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (input == 'stone' && nameWeapon == 'scissors') {
-    myOutputValue = 'hooray! ' + input + ' clobbers ' + nameWeapon + '. you win!';
+    winNum = winNum + 1;
+    myOutputValue = 'hooray! ' + input + ' clobbers ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
 
-  //LOSE
+  //LOSS
   if (nameWeapon == 'scissors' && input == 'paper') {
-    myOutputValue = 'sorry! ' + nameWeapon + ' shreds ' + input + '. you lose :(';
+    lossNum = lossNum + 1;
+    myOutputValue = 'sorry! ' + nameWeapon + ' shreds ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (nameWeapon == 'paper' && input == 'stone') {
-    myOutputValue = 'sorry! ' + nameWeapon + ' suffocates' + input + '. you lose :(';
+    lossNum = lossNum + 1;
+    myOutputValue = 'sorry! ' + nameWeapon + ' suffocates ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   if (nameWeapon == 'stone' && input == 'scissors') {
-    myOutputValue = 'sorry! ' + nameWeapon + ' clobbers ' + input + '. you lose :(';
+    lossNum = lossNum + 1;
+    myOutputValue = 'sorry! ' + nameWeapon + ' clobbers ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
   }
   return myOutputValue;
 }
 
 
 var randomWeapon = function () {
-  return 1;
+  return 2;
   var randomFloat = Math.random() * 3;
   var resultInteger = Math.floor(randomFloat) + 1;
   return resultInteger;
