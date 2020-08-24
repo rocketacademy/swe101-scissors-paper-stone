f908800955396a7f6c6f367e84482f054bb3d37d
Nuuggs <69680070+Nuuggs@users.noreply.github.com>
2020-08-24 10:28:38 +0800

    pseudo code written

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Disable ESLint rule no-unused-vars on next line because this code
 // is a learning tool and not a full-fledged node app.
 // eslint-disable-next-line no-unused-vars
+// scissors paper stone game: input vs. random selector -> define rules.
+// win-loss record: game state, track and output wins and losses -> output win percentage
+// user name: first input = store username -> default page (HTML), instructions to input username
+// -> after username input, initialize game, display message to start "input Scissors..."
+// input validation: limiting inputs to only 3 options
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
