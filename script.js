01211765617488e6bf30ab06c6c8657664d64c7e
Inna Abadilla <innaabadilla@Innas-MacBook-Air.local>
2020-08-25 11:51:15 +0800

    added username and input validation features

--- script.js
+++ script.js
@@ -10,77 +10,84 @@
 var drawNum = 0;
 var winNum = 0;
 var lossNum = 0;
-var invalidNum = 0;
-
-var main = function (input) {
-  var numWeapon = randomWeapon(); // 1 to 3
-  console.log(numWeapon); // Displays numWeapon in the console
-  var nameWeapon;
-  invalidNum = invalidNum + 1;
-
-
-  if (numWeapon == 1) {
-    nameWeapon = 'scissors'
-  };
-
-  if (numWeapon == 2) {
-    nameWeapon = 'paper'
-  };
-
-  if (numWeapon == 3) {
-    nameWeapon = 'stone'
-  };
-
-  var myOutputValue = 'ha! nice try! ' + input + ' is a bogus weapon. >:( you have made ' + invalidNum + ' invalid attempt/s.';
-
-  //DRAW
-  if (input == 'scissors' && nameWeapon == 'scissors') {
-    drawNum = drawNum + 1;
-    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (input == 'paper' && nameWeapon == 'paper') {
-    drawNum = drawNum + 1;
-    myOutputValue = ' draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (input == 'stone' && nameWeapon == 'stone') {
-    drawNum = drawNum + 1;
-    myOutputValue = 'draw! the both of us selected ' + nameWeapon + ' :) total draws: ' + (drawNum) + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-
-  //WIN
-  if (input == 'scissors' && nameWeapon == 'paper') {
-    winNum = winNum + 1;
-    myOutputValue = 'hooray! ' + input + ' shreds ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (input == 'paper' && nameWeapon == 'stone') {
-    winNum = winNum + 1;
-    myOutputValue = 'hooray! ' + input + ' suffocates ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (input == 'stone' && nameWeapon == 'scissors') {
-    winNum = winNum + 1;
-    myOutputValue = 'hooray! ' + input + ' clobbers ' + nameWeapon + '. you win! total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-
-  //LOSS
-  if (nameWeapon == 'scissors' && input == 'paper') {
-    lossNum = lossNum + 1;
-    myOutputValue = 'sorry! ' + nameWeapon + ' shreds ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (nameWeapon == 'paper' && input == 'stone') {
-    lossNum = lossNum + 1;
-    myOutputValue = 'sorry! ' + nameWeapon + ' suffocates ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  if (nameWeapon == 'stone' && input == 'scissors') {
-    lossNum = lossNum + 1;
-    myOutputValue = 'sorry! ' + nameWeapon + ' clobbers ' + input + '. you lose :( total draws: ' + drawNum + ' total wins: ' + winNum + ' total losses: ' + lossNum;
-  }
-  return myOutputValue;
-}
-
+var userName = '';
+var inputMode = 'username';
 
 var randomWeapon = function () {
-  return 2;
   var randomFloat = Math.random() * 3;
   var resultInteger = Math.floor(randomFloat) + 1;
   return resultInteger;
+};
+
+var main = function (input) {
+  if (inputMode == 'username') {
+    userName = input
+    inputMode = 'sps';
+    myOutputValue = 'hi, ' + userName + '! select your weapon to start ✂️📄💎'
+  } else if (inputMode == 'sps') {
+    var numWeapon = randomWeapon();
+    var nameWeapon;
+
+    if (numWeapon == 1) {
+      nameWeapon = 'scissors'
+    };
+
+    if (numWeapon == 2) {
+      nameWeapon = 'paper'
+    };
+
+    if (numWeapon == 3) {
+      nameWeapon = 'stone'
+    };
+
+    console.log(nameWeapon); // Displays nameWeapon in the console
+
+
+    //DRAW
+    if (input == 'scissors' && nameWeapon == 'scissors') {
+      drawNum = drawNum + 1;
+      myOutputValue = 'nooo, ' + userName + '! i chose ' + nameWeapon + ' first. neither of us gets a point 🤦‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (input == 'paper' && nameWeapon == 'paper') {
+      drawNum = drawNum + 1;
+      myOutputValue = 'nooo, ' + userName + '! i chose ' + nameWeapon + ' first. neither of us gets a point 🤦‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (input == 'stone' && nameWeapon == 'stone') {
+      drawNum = drawNum + 1;
+      myOutputValue = 'nooo, ' + userName + '! i chose ' + nameWeapon + ' first. neither of us gets a point 🤦‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+
+    //WIN
+    if (input == 'scissors' && nameWeapon == 'paper') {
+      winNum = winNum + 1;
+      myOutputValue = 'humph, ' + userName + '! ' + input + ' shred ' + nameWeapon + '. you win. 🙇‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (input == 'paper' && nameWeapon == 'stone') {
+      winNum = winNum + 1;
+      myOutputValue = 'humph, ' + userName + '! ' + input + ' suffocates ' + nameWeapon + '. you win. 🙇‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (input == 'stone' && nameWeapon == 'scissors') {
+      winNum = winNum + 1;
+      myOutputValue = 'humph, ' + userName + '! ' + input + ' crushes ' + nameWeapon + '. you win. 🙇‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+
+    //LOSS
+    if (nameWeapon == 'scissors' && input == 'paper') {
+      lossNum = lossNum + 1;
+      myOutputValue = 'sorry, ' + userName + '! ' + nameWeapon + ' shred ' + input + '. you lose. 🙅‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+
+    if (nameWeapon == 'paper' && input == 'stone') {
+      lossNum = lossNum + 1;
+      myOutputValue = 'sorry, ' + userName + '! ' + nameWeapon + ' suffocates ' + input + '. you lose. 🙅‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (nameWeapon == 'stone' && input == 'scissors') {
+      lossNum = lossNum + 1;
+      myOutputValue = 'sorry, ' + userName + '! ' + nameWeapon + ' crushes ' + input + '. you lose. 🙅‍♀️ ' + userName + ' score: ' + winNum + ' my score: ' + lossNum;
+    }
+    if (input != 'scissors' && input != 'paper' && input != 'stone') {
+      myOutputValue = 'did you try to encode a bogus weapon? try again!'
+    }
+  }
+  return myOutputValue;
 }
