74a28d1e7db4217a3d1e1211b92b22dc666c90ed
ShaneC <shanes_email_is@yahoo.com>
2020-08-25 16:00:59 +0800

    scissors paper stone initialized

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+//  set diceRoll to a maximum of 3 choices.
+//  will be pairing each number to a choice
+//  1. 'scissors'
+//  2. 'paper'
+//  3. 'stone'
+var computerSpsRoll = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  var spsNumber = randomInteger + 1;
+  return spsNumber;
+};
 // Disable ESLint rule no-unused-vars on next line because this code
 // is a learning tool and not a full-fledged node app.
 // eslint-disable-next-line no-unused-vars
 var main = function (input) {
   var myOutputValue = 'hello world';
+  var spsRoll = computerSpsRoll();
+  console.log(spsRoll);
+
+  if (computerSpsRoll == 1) {
+    spsRoll = 'scissors';
+  }
+
+  if (computerSpsRoll == 2) {
+    spsRoll = 'paper';
+  }
+
+  if (computerSpsRoll == 3) {
+    spsRoll = 'stone';
+  }
+
+  var myOutputValue = 'you lost';
+
   return myOutputValue;
 };
