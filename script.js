a3b674f32bc2409502a50fc95d781268b7a464d5
Paolo Loanzon <paolo@bukas.ph>
2020-08-25 17:14:05 +0800

    main game working now

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ var main = function (input) {
   myOutputValue = 'empty';
   var HandRef = diceRoll;
   var autohand = 'empty'
-  if (input !== 'stone' || input !== 'scissors'|| input !== 'paper'){myOutputValue = 'Invalid'}
-
-  if(HandRef = 1){autohand = 'paper'};
-  if(HandRef = 2){autohand = 'scissors'};
-  if(HandRef = 3){autohand = 'stone'};
+  if (input !== 'stone' || input !== 'scissors'|| input !== 'paper'){myOutputValue = 'Invalid';}
+  if(HandRef = 1){autohand = 'paper';};
+  if(HandRef = 2){autohand = 'scissors';};
+  if(HandRef = 3){autohand = 'stone';}
   var result = 'Empty';
   //draw outcome
-  if (input == autohand){draw = draw + 1; result = 'draw'};
+  if (input == autohand){draw = draw + 1; result = 'draw';}
   // loss outcome
-  if (input =='paper' && autohand == 'scissors'){result = 'lose'};
-  if (input =='stone' && autohand == 'paper'){result = 'lose'};
-  if (input =='scissors' && autohand == 'stone'){result = 'lose'};
+  if (input =='paper' && autohand == 'scissors'){result = 'lose';}
+  if (input =='stone' && autohand == 'paper'){result = 'lose';}
+  if (input =='scissors' && autohand == 'stone'){result = 'lose';}
   //win outcome
-  if (input =='paper' && autohand == 'stone'){result = 'win' };
-  if (input =='stone' && autohand == 'scissors'){result = 'win'};
-  if (input =='scissors' && autohand == 'paper'){result = 'win'};
-  if (result == 'loss') {loss = loss+1};
-  if (result == 'draw') {draw = draw +1};
-  if (result == 'win') {win = win +1};
-//  var myOutputValue = 'You chose '+ input + ' and the computer chose ' + autohand + '. You '+ result + '! Your current tally is ' + win + '- wins, ' + loss + '- loss, ' + win + '- draws.';
-var myOutputValue = result;
+  if (input =='paper' && autohand == 'stone'){result = 'win';}
+  if (input =='stone' && autohand == 'scissors'){result = 'win';}
+  if (input =='scissors' && autohand == 'paper'){result = 'win';}
+  if (result == 'loss') {loss = loss+1;}
+  if (result == 'draw') {draw = draw +1;}
+  if (result == 'win') {win = win +1;}
+var myOutputValue = 'You chose '+ input + ' and the computer chose ' + autohand + '. You '+ result + '! Your current tally is ' + win + '- wins, ' + loss + '- loss, ' + win + '- draws.';
+//var myOutputValue = result;
 //  var myOutputValue =  win;
   return myOutputValue;
 };
