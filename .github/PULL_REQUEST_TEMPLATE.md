fa30ad220f437588f03eedc9772f0a74fe64f715
Kenrickles <kenrickles@gmail.com>
2020-08-25 18:30:42 +0800

    This is my work.

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -3,15 +3,21 @@ Please fill out the survey before submitting the pull request. Thanks!
 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 
 How many hours did you spend on this assignment?
+1 hour
+
 
 Please fill in one error and/or error message you recieved while working on this assignment.
+Expected Syntax
 
 What part of the assignment did you spend the most time on?
+Solving the error
 
-Comfort Level (1-5):
+Comfort Level (1-5): 4 
 
-Completeness Level (1-5):
+Completeness Level (1-5):3 
 
 What did you think of this deliverable?
+As i was catching up with previous lesson, would need more time to complete it to its full level
 
 Is there anything in this code that you feel pleased about?
+Not really
--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 // Disable ESLint rule no-unused-vars on next line because this code
 // is a learning tool and not a full-fledged node app.
 // eslint-disable-next-line no-unused-vars
-var main = function (input) {
+
+// Adding Win-lost record for the life cycle of program 
+  var noOfLosses = 0;
+  var noOfWins = 0;
+  var noOfDraws = 0;
+  var noOfGames = 0;
+  var userName = 'blank';
+
+// Randomising computerChoice of rock/paper/scissors
+  var getComputerChoice = function() {
+    var randomNumber = Math.random() * 3; // Getting a random number
+    var randomChoice = Math.floor(randomNumber) + 1; // Making this a whole number
+    if (randomChoice == 1){
+      return 'rock';
+    }
+    if (randomChoice == 2){
+      return 'scissors';
+    }
+    if (randomChoice == 3){
+      return 'paper'
+    }
+    return getComputerChoice
+  };
+
+var main = function (userChoice) {
+  noOfGames += 1;
   var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+  var computerChoice = getComputerChoice();
+  var outcome; 
+   // Draw scenario
+  if (userChoice == computerChoice) {
+      noOfDraws += 1;
+      outcome = ' Its a draw ';
+    }
+    // Lose scenario
+  if (userChoice == 'rock' && computerChoice == 'paper' || 
+      userChoice == 'paper' && computerChoice == 'scissors' || 
+      userChoice == 'scissors' && computerChoice == 'rock') {
+       noOfLosses += 1
+       outcome = " You lose :( ";
+      }
+
+    // Win scenario
+  if (userChoice == 'rock' && computerChoice == 'scissors' ||
+     userChoice == 'scissors' && computerChoice == 'paper' ||
+     userChoice == 'paper' && computerChoice == 'rock') {
+       noOfWins += 1;
+       outcome = " You won!!!! ";
+     }
+     myOutputValue = 'Number of wins = ' + noOfWins + ' Number of losses = ' + noOfLosses + 
+     ' Number of Draw = ' + noOfDraws + ' Total number of games = ' + noOfGames + ' You chose ' + userChoice +
+     '. The computer chose '  + computerChoice + '. ' + outcome + ' please try again '
+    return (myOutputValue); }
+  
+  
