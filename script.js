3f3cb057074e149064785f74de830e304745ab0e
Veena <veenasivan@gmail.com>
2020-08-26 10:10:45 +0800

    Added Reverse mode of the game

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ var countDraw = 0; // Tracks the number of times the game was a draw.
 var strUserName = ''; // Variable to store the name of the user
 var strMostRecentWinner = ''; // Variable to store the most recent winner.
 var varSPSGameType = 0; // Variable to store the type of game.
+var varGameMode = 0; // Variable to store the mode of the game. 1 == Normal SPS, 2 == Reverse mode
+
 // Variable to store the count of winning in Korean SPS game
 var countUserWinKSPS = 0;
 var countProgramWinKSPS = 0;
@@ -35,6 +37,15 @@ var printGameOptionMessage = function ()
   var varMessage = 'Please choose the type of game to play: <br/>';
   varMessage += 'Enter 1 for playing normal Scissors Paper Stone game. <br/>';
   varMessage += 'Enter 2 for playing Koren Scissors Paper Stone game (muk-jji-ppa). <br/>';
+  return varMessage;
+};
+
+// Function that returns the game mode options to print
+var printGameModeMessage = function ()
+{
+  var varMessage = 'Please choose the <b>Mode</b> of game to play: <br/>';
+  varMessage += 'Enter 1 for playing Normal Scissors Paper Stone game. <br/>';
+  varMessage += 'Enter 2 for playing Reversed Scissors Paper Stone game. <br/>';
   return varMessage;
 };
 
@@ -85,6 +96,13 @@ var getGameResult = function (userGuess) {
   // get the random draw and mapping between the random number and values are:
   // 1 ==> scissors, 2 ==> paper and 3 ==> stone
   var randomDraw = getRandomNumber(3);
+
+  // to delete
+  console.log('User guessed: ' + userGuess);
+  if(randomDraw == 1 ) {
+    console.log('Program guessed: scissors'); }else if(randomDraw == 2) {
+    console.log('Program guessed: paper'); }else{ 
+    console.log('Program guessed: stone'); }
   
   // If the user input and program output is same, the result is a "draw".
   if (((userGuess == varScissors) && (randomDraw == 1))
@@ -93,23 +111,43 @@ var getGameResult = function (userGuess) {
     varGameResult = varDraw;
     countDraw += ( varSPSGameType == 1 ) ? 1 : 0;
   }
-  // check for the cases where user wins.
+  // check for the cases where user wins in normal mode of play, 
+  // while program wins in the reverse mode.
   else if (((userGuess == varScissors) && (randomDraw == 2)) // User has scissors, program has paper
           || ((userGuess == varPaper) && (randomDraw == 3)) // user has Paper, program has stone
           || ((userGuess == varStone) && (randomDraw == 1))) // User has Stone, program has Scissors
   {
-    varGameResult = varUser;
-    strMostRecentWinner = strUserName;
-    countUserWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    if(varGameMode == 1)
+    {
+      varGameResult = varUser;
+      strMostRecentWinner = strUserName;
+      countUserWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    }
+    else
+    {
+      varGameResult = varProgram;
+      strMostRecentWinner = 'Program';
+      countProgramWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    }
   }
-  // check for the cases where the Program wins
+  // check for the cases where the Program wins in the normal mode of play,
+  // while user wins in the reverse mode
   else if (((userGuess == varScissors) && (randomDraw == 3)) // User has scissors, program has Stone
           || ((userGuess == varPaper) && (randomDraw == 1)) // user has Paper, program has Scissors
           || ((userGuess == varStone) && (randomDraw == 2))) // User has Stone, program has Paper
   {
-    varGameResult = varProgram;
-    strMostRecentWinner = 'Program';
-    countProgramWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    if( varGameMode == 1)
+    {
+      varGameResult = varProgram;
+      strMostRecentWinner = 'Program';
+      countProgramWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    }
+    else
+    {
+      varGameResult = varUser;
+      strMostRecentWinner = strUserName;
+      countUserWon += ( varSPSGameType == 1 ) ? 1 : 0;
+    }
   }
   else {
     varGameResult = 'Inavlid Input. <br/>Please input one of the following options: "scissors", "paper" or "stone". <br />';
@@ -180,8 +218,28 @@ var main = function (userInput) {
   if( varSPSGameType == 0 )
   {
     varSPSGameType = Number( userInput );
+    return printGameModeMessage();
+  }
+
+  if(( varGameMode == 0 ) && ((userInput <= 0 || userInput > 2) || isNaN(Number( userInput ))))
+  {
+    varGameMode = 0;
+    return printGameModeMessage();
+  }
+  if( varGameMode == 0 )
+  {
+    varGameMode = Number( userInput );
     return ('Start playing the game by entering one of the options: "scissors", "paper" or "stone". ');
-  }  
+  }
+
+  // to delete:
+  if(varSPSGameType == 1) {
+    console.log('Normal SPS game is selected'); } else  {
+    console.log( 'Korean SPS is selected' );  }
+  if( varGameMode == 1)  {
+    console.log( 'Normal mode of play is selected' );  }  else{
+    console.log( 'Reverse mode of play is selected' );  }
+
   // Validate the input specified by user for playing the game
   if(!validateInput(userInput))
   {    
