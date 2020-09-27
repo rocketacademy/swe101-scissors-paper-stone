600673d8862de271f314d1962e0b4db9f008c450
jeremylim91 <jeremylim_91@live.com>
2020-09-28 01:40:45 +0800

    added features up till the validation of the user input

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 //state-- score counter
 var scoreCounterUser = 0;
 var scoreCounterComputer = 0;
@@ -17,38 +19,60 @@ var getRandomWord = function () {
   return randomWord;
 }
 
-var main = function (input) {
-  //get a randomWord through a randomNumber
-  var SPSComputer = getRandomWord();
-  console.log('computer throws SPS');
-  console.log(SPSComputer);
-  //compare randomWord against the user input
+//Game modes
+var currentGameMode = 'waiting for user name';
+var userName = '';
 
-  //User will win if (user throw paper and RandomWord is Stone) OR (user throws stone and the randomWOrd is scissors) OR (user throws Scissors and the randomWord is paper)
-  if ((input == 'scissors' && SPSComputer == 'paper') || (input == 'paper' && SPSComputer == 'stone') || (input == 'stone' && SPSComputer == 'scissors')) {
-    console.log('user wins');
-    scoreCounterUser = scoreCounterUser + 1;
-    myOutputValue = 'You win! The computer threw a ' + SPSComputer + ' and you threw a ' + input +
-      '<br><br> SCORE' +
-      '<br> You: ' + scoreCounterUser +
-      '<br> Computer: ' + scoreCounterComputer;
-  }
-  //else if both throw the same, it's a draw
-  else if (input == SPSComputer) {
-    console.log('user draws w computer');
-    myOutputValue = 'It is a draw! You both threw a ' + input +
-      '<br><br> SCORE' +
-      '<br> You: ' + scoreCounterUser +
-      '<br> Computer: ' + scoreCounterComputer;
-  }
-  //else the user loses
-  else {
-    console.log('user loses')
-    scoreCounterComputer = scoreCounterComputer + 1;
-    myOutputValue = 'You lose. The computer threw a ' + SPSComputer + ' and you threw a ' + input +
-      '<br><br> SCORE' +
-      '<br> You: ' + scoreCounterUser +
-      '<br> Computer: ' + scoreCounterComputer;
+var main = function (input) {
+  var myOutputValue = '';
+
+  //set game mode to default mode
+  if (currentGameMode == 'waiting for user name') {
+    userName = input;
+    // now that we have name, switch the game mode
+    currentGameMode = 'SPS mode';
+    myOutputValue = 'Hello, ' + userName +
+      '<br> This game is called  \'scissors, paper, stone\'. Please input \'scissors\', \'paper\', or \'stone\' into the box above';
+
+  } else if (currentGameMode == 'SPS mode') {
+    //validate userinput using function
+    if (!((input == 'scissors') || (input == 'paper') || (input == 'stone'))) {
+      myOutputValue = 'please input either \'scissors\', \'paper\', or\'stone\'only';
+    } else {
+      //get a randomWord through a randomNumber
+      var SPSComputer = getRandomWord();
+      console.log('computer throws SPS');
+      console.log(SPSComputer);
+      //compare randomWord against the user input
+
+      //User will win if (user throw paper and RandomWord is Stone) OR (user throws stone and the randomWOrd is scissors) OR (user throws Scissors and the randomWord is paper)
+      if ((input == 'scissors' && SPSComputer == 'paper') || (input == 'paper' && SPSComputer == 'stone') || (input == 'stone' && SPSComputer == 'scissors')) {
+        console.log('user wins');
+        scoreCounterUser = scoreCounterUser + 1;
+        myOutputValue = 'You win, ' + userName + '! The computer threw a ' + SPSComputer + ' and you threw a ' + input +
+          '<br><br> SCORE' +
+          '<br> You: ' + scoreCounterUser +
+          '<br> Computer: ' + scoreCounterComputer;
+      }
+      //else if both throw the same, it's a draw
+      else if (input == SPSComputer) {
+        console.log('user draws w computer');
+        myOutputValue = 'It\'s a draw, ' + userName + '! You both threw a ' + input +
+          '<br><br> SCORE' +
+          '<br> You: ' + scoreCounterUser +
+          '<br> Computer: ' + scoreCounterComputer;
+      }
+      //else the user loses
+      else {
+        console.log('user loses')
+        scoreCounterComputer = scoreCounterComputer + 1;
+        myOutputValue = 'You lose, ' + userName + '. The computer threw a ' + SPSComputer + ' and you threw a ' + input +
+          '<br><br> SCORE' +
+          '<br> You: ' + scoreCounterUser +
+          '<br> Computer: ' + scoreCounterComputer;
+      }
+    }
   }
   return myOutputValue;
+
 };
