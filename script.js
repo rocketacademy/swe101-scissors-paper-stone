d8b587689ffe60b5cee26c12b9f664ebace891a2
roldroldxz <roldroldxz@live.com.sg>
2021-01-25 12:43:37 +0800

    updating project

--- script.js
+++ script.js
@@ -1,4 +1,58 @@
+// create random dice function for a total result of 3
+var randomDiceRoll = function () {
+  // return 2;
+  var randomBetNumber = Math.random() * 3;
+  var randomInterger = Math.floor(randomBetNumber);
+  var randomResult = randomInterger + 1;
+  return randomResult;
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  // 3 different random generated outcome assigned for scissors, paper, stone
+  var randomNumber = randomDiceRoll();
+  var randomWord;
+  if (randomNumber == 1) {
+    randomWord = 'scissors';
+  }
+  else if (randomNumber == 2) {
+    randomWord = 'paper';
+  }
+  else if (randomNumber == 3) {
+    randomWord = 'stone';
+  }
+  console.log(randomNumber);
+  console.log(randomWord);
+
+  var myOutputValue = 'invalid input';
+  // 3 different inputs, scissors paper stone
+  if (input == randomWord) {
+    myOutputValue = 'you chose ' + input + ' and its a draw! try again!';
+  }
+  // if input is scissors
+
+  else if (input == 'scissors' && randomWord == 'paper') {
+    myOutputValue = 'you chose ' + input + ' and won ' + randomWord + ' !';
+  }
+  else if (input == 'scissors' && randomWord == 'stone') {
+    myOutputValue = 'you chose ' + input + ' and lost to ' + randomWord + ', please try again!';
+  }
+  // if input is paper
+
+  else if (input == 'paper' && randomWord == 'stone') {
+    myOutputValue = 'you chose ' + input + ' and won ' + randomWord + ' !';
+  }
+  else if (input == 'paper' && randomWord == 'scissors') {
+    myOutputValue = 'you chose ' + input + ' and lost to ' + randomWord + ', please try again!';
+  }
+  // if input is stone
+
+  else if (input == 'stone' && randomWord == 'scissors') {
+    myOutputValue = 'you chose ' + input + ' and won ' + randomWord + ' !';
+  }
+  else if (input == 'stone' && randomWord == 'paper') {
+    myOutputValue = 'you chose ' + input + ' and lost to ' + randomWord + ', please try again!';
+  }
+  console.log(randomWord);
+
   return myOutputValue;
 };
