563e42533ce6008db63eff68eaa229950695883c
YOUR_GITHUB_vincentworld1 <YOUR_GITHUB_vincentmoh63@gmail.com>
2021-03-08 14:27:04 +0800

    Created random numbers and assigned numbers to word

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+// random number from 0 to 2
+var randomNum = function () {
+  var numZeroTotwo = Math.floor(Math.random() * 3);
+  return numZeroTotwo;
+};
+// assign value to words
+var assignNumberToWords = function () {
+  var numGenerator = randomNum();
+  if (numGenerator == 0) {
+    return 'scissor';
+  }
+  if (numGenerator == 1) {
+    return 'rock';
+  }
+  if (numGenerator == 2) {
+    return 'paper';
+  }
+  return 'got bug';
+};
+
 var main = function (input) {
+  var scissorPaperStone = assignNumberToWords();
+  console.log('scissor-paper-stone');
+  console.log(scissorPaperStone);
   var myOutputValue = 'hello world';
-  return myOutputValue;
+  if (input == scissorPaperStone) {
+    myOutputValue = 'you win';
+  } return myOutputValue;
 };
