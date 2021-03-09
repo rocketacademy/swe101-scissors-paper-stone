fdc3e344f17d5ec3bf02019a6d424d2d1a85f44f
ts-oh <tsohlove@gmail.com>
2021-03-09 22:28:16 +0900

    Finished, added username input and validation and score keeping

--- script.js
+++ script.js
@@ -1,64 +1,74 @@
 // Unfinished Project 1
 
 var currentGameMode = 'waiting for user name mode';
-// var userScore = 0;
 var userName = '';
 
-// Score Keeping, intial score is 0 (Global State).
-// var scoreUserWin = 0;
-// var scoreCpuWin = 0;
-// var scoreDraws = 0;
-// var scoreKeep = function () {
-//   return ''
-// };
+var scoreUserWin = 0;
+var scoreCpuWin = 0;
+var scoreDraw = 0;
 
 // Get random CPU pick from an array
 var cpuPick = function () {
-  var pickList = ['rock', 'scissors', 'paper'];
-  var cpuPick = pickList[Math.floor(Math.random() * pickList.length)];
-  console.log('computer picked ' + cpuPick + '!!!');
-  return cpuPick;
+  var pickList = ['rock', 'scissor', 'paper'];
+  var randPick = pickList[Math.floor(Math.random() * pickList.length)];
+  console.log('computer picked ' + randPick + '!!!');
+  return randPick;
 };
 
 var main = function (input) {
   var myOutputValue = '';
-
   // do this if game mode is waiting for user...
   if (currentGameMode == 'waiting for user name mode') {
     // set the user name
     userName = input;
-
+    // if user does not type anything
+    if (userName == '') {
+      return 'not a valid user name';
+    }
     // now that we have user name, switch the mode
-    currentGameMode = 'dice game mode';
+    currentGameMode = 'game mode';
 
-    myOutputValue = 'Hello ' + userName;
-  } else if (currentGameMode == 'dice game mode') {
+    myOutputValue = 'Hello ' + userName + " let's play RSP";
+    return myOutputValue;
+  } if (currentGameMode == 'game mode') {
     // game logic here
     var userInput = input;
     var randomCpuPick = cpuPick();
-    var myOutputValue = 'Your pick is ' + input + '. CPU pick is ' + randomCpuPick + '!';
-    if (input != 'rock' || 'paper' || 'scissor') {
+    myOutputValue = 'Your pick is ' + input + '. CPU pick is ' + randomCpuPick + '!';
+    console.log(input);
+    console.log((input != 'rock' && input != 'paper' && input != 'scissor'));
+    // input not equal to rock, and not to paper, and not to scissor
+    if (input != 'rock' && input != 'paper' && input != 'scissor') {
       return 'This is not a valid input, please enter only the word "rock", "paper", "scissor"';
     }
+    // var compareGame = function (randomCpuPick, userInput) {}
     if (randomCpuPick == userInput) {
-      return myOutputValue + " it's a tie";
+      scoreDraw = scoreDraw + 1;
+      console.log(scoreDraw);
+      return myOutputValue + " it's a tie. Scoreboard: " + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     } if (randomCpuPick == 'rock' && userInput == 'paper') {
-      return myOutputValue + ' You win!';
+      scoreUserWin = scoreUserWin + 1;
+      return myOutputValue + ' You win! Scoreboard:' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
-    if (randomCpuPick == 'rock' && userInput == 'scissors') {
-      return myOutputValue + ' You lose!';
+    if (randomCpuPick == 'rock' && userInput == 'scissor') {
+      scoreCpuWin = scoreCpuWin + 1;
+      return myOutputValue + ' You lose! Scoreboard: ' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
-    if (randomCpuPick == 'paper' && userInput == 'scissors') {
-      return myOutputValue + ' You win!';
+    if (randomCpuPick == 'paper' && userInput == 'scissor') {
+      scoreUserWin = scoreUserWin + 1;
+      return myOutputValue + ' You win! Scoreboard: ' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
     if (randomCpuPick == 'paper' && userInput == 'rock') {
-      return myOutputValue + ' You lose!';
+      scoreCpuWin = scoreCpuWin + 1;
+      return myOutputValue + ' You lose! Scoreboard: ' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
-    if (randomCpuPick == 'scissors' && userInput == 'rock') {
-      return myOutputValue + ' You win!';
+    if (randomCpuPick == 'scissor' && userInput == 'rock') {
+      scoreUserWin = scoreUserWin + 1;
+      return myOutputValue + ' You win! Scoreboard: ' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
-    if (randomCpuPick == 'scissors' && userInput == 'paper') {
-      return myOutputValue + ' You lose!';
+    if (randomCpuPick == 'scissor' && userInput == 'paper') {
+      scoreCpuWin = scoreCpuWin + 1;
+      return myOutputValue + ' You lose! Scoreboard: ' + 'D:' + scoreDraw + ' W:' + scoreUserWin + ' L:' + scoreCpuWin;
     }
     return myOutputValue;
   }
