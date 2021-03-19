487c9da75258fa1ea5093ad5c4812bed3a2ad84d
shayaank243 <rskharal@icloud.com>
2021-03-19 22:55:33 +0800

    this is my updated version-- sorry for the late upload

--- script.js
+++ script.js
@@ -1,68 +1,85 @@
+var winCount = 0;
+var lossCount = 0;
+var tieCount = 0;
+var mode = 'waiting for player to input his username';
+console.log(mode);
+var userName = '';
+var startingMessage = 'hello! what is your name!';
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  if (input == '' || isNaN(Number(input)) == false) {
+    console.log('a');
+    return 'please input a proper username';
+  }
+  if (mode == 'game mode' && input != 'scissors' && input != 'paper' && input != 'stone') {
+    console.log('c');
+    return 'please enter either scissors, paper or stone';
+  }
+
+  if (mode == 'waiting for player to input his username') {
+    userName = input;
+    mode = 'game mode';
+    console.log('b');
+    return 'hi ' + userName + ' lets play the game!';
+  }
   var words = computerGenResult();
-  if(input == 'scissors' && words == 2){
-    console.log('scissors vs paper')
-    console.log('scissors win')
-    return 'you won!!';
+  if (input == 'scissors' && words == 2) {
+    console.log('scissors vs paper');
+    console.log('scissors win');
+    winCount = winCount + 1;
+    return 'you won!! W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'scissors' && words == 3){
-    console.log('scissors v stone')
-    console.log('stone wins')
-    return 'you lost';
+  if (input == 'scissors' && words == 3) {
+    console.log('scissors v stone');
+    console.log('stone wins');
+    lossCount = lossCount + 1;
+    return 'you lost W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'scissors' && words == 1){
-    console.log('both words are the same, its a draw')
-    return 'It is a draw!!'
-  }  
-  if(input == 'paper' && words == 1){
-    console.log('paper vs scissors')
-    console.log('paper loses')
-    return 'you lost';
+  if (input == 'scissors' && words == 1) {
+    console.log('both words are the same, its a draw');
+    tieCount = tieCount + 1;
+    return 'It is a draw!! Both words are the same! W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'paper' && words == 3){
-    console.log('paper v stone')
-    console.log('paper wins')
-    return 'you win!!'
+  if (input == 'paper' && words == 1) {
+    console.log('paper vs scissors');
+    console.log('paper loses');
+    lossCount = lossCount + 1;
+    return 'you lost!! W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'paper' && words == 2){
-    console.log('It is a draw both words are the same')
-    return ' it is a draw! both words are the same'
-  }  
-  
-  if(input == 'stone' && words == 1){
-    console.log('stone v scissors')
-    console.log('stone wins')
-    return 'you win!!'
+  if (input == 'paper' && words == 3) {
+    console.log('paper v stone');
+    console.log('paper wins');
+    winCount = winCount + 1;
+    return 'you won!! W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'stone' && words == 2){
-    console.log('stone v paper')
-    console.log(' paper wins, stone loses')
-    return ' you lose';
+  if (input == 'paper' && words == 2) {
+    console.log('It is a draw both words are the same');
+    tieCount = tieCount + 1;
+    return ' it is a draw! both words are the same W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
   }
-  if(input == 'stone' && words == 3){
-    console.log('both words are the same')
-    console.log('its a draw')
-    return 'it is a draw!! Both words guessed are the same';
-  }  
-    
-};       
-computerGenResult = function(){
-var randomNumber = Math.random() * 3
-var randomNumberROff = Math.floor(randomNumber)
-var finalNumber = randomNumberROff + 1
-return finalNumber;
-}
 
-    
+  if (input == 'stone' && words == 1) {
+    console.log('stone v scissors');
+    console.log('stone wins');
+    winCount = winCount + 1;
+    return 'you won!! W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
+  }
+  if (input == 'stone' && words == 2) {
+    console.log('stone v paper');
+    console.log(' paper wins, stone loses');
+    lossCount = lossCount + 1;
+    return ' you lose W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
+  }
+  if (input == 'stone' && words == 3) {
+    console.log('both words are the same');
+    console.log('its a draw');
+    tieCount = tieCount + 1;
+    return 'it is a draw!! Both words guessed are the same W:' + winCount + ' L:' + lossCount + ' D:' + tieCount;
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
+var computerGenResult = function () {
+  var randomNumber = Math.random() * 3;
+  var randomNumberROff = Math.floor(randomNumber);
+  var finalNumber = randomNumberROff + 1;
+  return finalNumber;
+};
