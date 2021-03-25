be4bdd9f34bba5b69250441a76310da45e35895f
jokatty <jyotikattani@gmail.com>
2021-03-25 11:26:43 +0800

    refactor repeated code as per feedback in last PR

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
 // function to generate random number
-function random(max) {
+function generateRandomNum(max) {
   let randomNum = Math.floor(Math.random() * max) + 1;
   return randomNum;
 }
 // create function to generate random item
-function randomOption() {
-  let randomItemNum = random(3);
+function generateRandomOption() {
+  let randomItemNum = generateRandomNum(3);
   let item;
   if (randomItemNum == 1) {
     item = 'scissors';
@@ -20,91 +19,44 @@ function randomOption() {
   return item;
 }
 
-//**STAGE-2 Win-Loss Record */
+// STAGE-2 Win-Loss Record 
 let winningCountComp = null;
 let winningCountPlayer = null;
 let drawCount = null;
-// **
 
 let recentWinner = 'yet to decide. keep playing.';
-
 // play normal SPS
 function playNormal(randomItem, input) {
-  let result;
-  if ((randomItem == 'scissors') && (input == 'paper')) {
+  let result = `computer chose ${randomItem} <br> and you chose ${input}. <br>`;
+  if (((randomItem == 'scissors') && (input == 'paper')) || ((randomItem == 'paper') && (input == 'stone')) || ((randomItem == 'stone') && (input == 'scissors'))) {
     winningCountComp += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 📃.</p> COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
-
+    result += `COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
   }
-  else if ((randomItem == 'scissors') && (input == 'stone')) {
+  else if (((randomItem == 'scissors') && (input == 'stone')) || ((randomItem == 'paper') && (input == 'scissors')) || ((randomItem == 'stone') && (input == 'paper'))) {
     winningCountPlayer += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 💎. YOU win!. 🏆</p> so far, ${userName} won ${winningCountPlayer} number of times.`;
-
-  }
-  else if ((randomItem == 'paper') && (input == 'stone')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem} 📃 <p> and you chose ${input} 💎. COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
-  }
-  else if ((randomItem == 'paper') && (input == 'scissors')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 📃 <p>and you chose ${input} ✄. YOU win! 🏆. </p>so far, ${userName} won ${winningCountPlayer} number of times.`;
-
-  }
-  else if ((randomItem == 'stone') && (input == 'scissors')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem}  💎 <p>and you chose ${input}. </p>COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
-
-  }
-  else if ((randomItem == 'stone') && (input == 'paper')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 💎 <p>and you chose ${input}. </p>YOU win! 🏆. so far, ${userName} won ${winningCountPlayer} number of times.`;
-
+    result += `YOU win!. 🏆</p> so far, ${userName} won ${winningCountPlayer} number of times.`;
   }
   else if (((randomItem == 'scissors') && (input == 'scissors')) || ((randomItem == 'paper') && (input == 'paper')) || ((randomItem == 'stone') && (input == 'stone'))) {
     drawCount += 1;
     result = `computer chose ${randomItem} and you chose ${input}. <p>It's a DRAW!.</p>so far ${drawCount} number of draws.`;
   }
-
   return result;
 }
 
-
 // play reverse SPS
 function playReverse(randomItem, input) {
-  let result;
-  if ((randomItem == 'scissors') && (input == 'paper')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 📃.YOU win!. 🏆</p> so far, ${userName} won ${winningCountPlayer} number of times.`;
-
-  }
-  else if ((randomItem == 'scissors') && (input == 'stone')) {
+  let result = `computer chose ${randomItem}  <p>and you chose ${input}.<br>`;
+  if (((randomItem == 'scissors') && (input == 'paper')) || ((randomItem == 'paper') && (input == 'stone')) || ((randomItem == 'stone') && (input == 'scissors'))) {
     winningCountPlayer += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 💎. </p> COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
-
+    result += `YOU win!. 🏆</p> so far, ${userName} won ${winningCountPlayer} number of times.`;
   }
-  else if ((randomItem == 'paper') && (input == 'stone')) {
+  else if (((randomItem == 'scissors') && (input == 'stone')) || ((randomItem == 'paper') && (input == 'scissors')) || ((randomItem == 'stone') && (input == 'paper'))) {
     winningCountComp += 1;
-    result = `computer chose ${randomItem} 📃 <p> and you chose ${input} 💎.YOU win! 🏆. </p>so far, ${userName} won ${winningCountPlayer} number of times. `;
-
-  }
-  else if ((randomItem == 'paper') && (input == 'scissors')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 📃 <p>and you chose ${input} ✄. COMPUTER wins! 💻. so far won ${winningCountComp} number of times. `;
-
-  }
-  else if ((randomItem == 'stone') && (input == 'scissors')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem}  💎 <p>and you chose ${input}. YOU win! 🏆. so far, ${userName} won ${winningCountPlayer} number of times.`;
-
-  }
-  else if ((randomItem == 'stone') && (input == 'paper')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 💎 <p>and you chose ${input}. </p>COMPUTER wins! 💻. so far won ${winningCountComp} number of times.</p>`;
-
+    result += `COMPUTER wins! 💻. so far won ${winningCountComp} number of times.`;
   }
   else if (((randomItem == 'scissors') && (input == 'scissors')) || ((randomItem == 'paper') && (input == 'paper')) || ((randomItem == 'stone') && (input == 'stone'))) {
     drawCount += 1;
-    result = `computer chose ${randomItem} and you chose ${input}. <p>It's a DRAW!.</p>so far ${drawCount} number of draws.`;
+    result += `It's a DRAW!.</p>so far ${drawCount} number of draws.`;
   }
   console.log(recentWinner);
   return result;
@@ -112,107 +64,75 @@ function playReverse(randomItem, input) {
 
 // korean version of the game
 function koreanVersion(randomItem, input) {
-  let result;
-  if ((randomItem == 'scissors') && (input == 'paper')) {
+  let result = `computer chose ${randomItem} <br> and you chose ${input}.`;
+  if (((randomItem == 'scissors') && (input == 'paper')) || ((randomItem == 'paper') && (input == 'stone')) || ((randomItem == 'stone') && (input == 'scissors'))) {
     winningCountComp += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 📃.</p> COMPUTER wins! 💻. `;
+    result += 'COMPUTER wins! 💻.';
     recentWinner = 'Computer';
   }
-  else if ((randomItem == 'scissors') && (input == 'stone')) {
+  else if (((randomItem == 'scissors') && (input == 'stone')) || ((randomItem == 'paper') && (input == 'scissors')) || ((randomItem == 'stone') && (input == 'paper'))) {
     winningCountPlayer += 1;
-    result = `computer chose ${randomItem} ✄ <p>and you chose ${input} 💎. YOU win!. 🏆</p> `;
-    recentWinner = 'Player';
-  }
-  else if ((randomItem == 'paper') && (input == 'stone')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem} 📃 <p> and you chose ${input} 💎. COMPUTER wins! 💻.`;
-    recentWinner = 'Computer';
-  }
-  else if ((randomItem == 'paper') && (input == 'scissors')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 📃 <p>and you chose ${input} ✄. YOU win! 🏆. </p>`;
-    recentWinner = 'Player';
-  }
-  else if ((randomItem == 'stone') && (input == 'scissors')) {
-    winningCountComp += 1;
-    result = `computer chose ${randomItem}  💎 <p>and you chose ${input}. </p>COMPUTER wins! 💻. `;
-    recentWinner = 'Computer';
-  }
-  else if ((randomItem == 'stone') && (input == 'paper')) {
-    winningCountPlayer += 1;
-    result = `computer chose ${randomItem} 💎 <p>and you chose ${input}. </p>YOU win! 🏆. `;
+    result += 'YOU win!. 🏆 ';
     recentWinner = 'Player';
   }
   else if (((randomItem == 'scissors') && (input == 'scissors')) || ((randomItem == 'paper') && (input == 'paper')) || ((randomItem == 'stone') && (input == 'stone'))) {
     drawCount += 1;
-    result = `computer chose ${randomItem} and you chose ${input}. <p>It's a DRAW!.</p>so far ${drawCount} number of draws. The ULTIMATE WINNER is ${recentWinner}.`;
+    result = `It's a DRAW!.</p>so far ${drawCount} number of draws. The ULTIMATE WINNER is ${recentWinner}.`;
   }
   console.log(recentWinner);
   return result;
 }
-
 // tracking state of the game
-let mode = 'normal';
-
+function setMode(userInput) {
+  let mode;
+  if (userInput == '') {
+    mode = 'normal';
+  }
+  else if (userInput == 'reverse') {
+    mode = 'reverse';
+  }
+  else if (userInput == 'normal') {
+    mode = 'normal';
+  }
+  else if (userInput == 'muk-jji-ppa') {
+    mode = 'muk-jji-ppa';
+  }
+  else if (userInput == 'computer') {
+    mode = 'computer';
+  }
+  return mode;
+}
+let mode = 'set the mode';
 
 var main = function (input) {
   let gameResult;
-  if (input == 'reverse') {
-    mode = 'reverse';
-    return gameResult = 'reverse mode set. play the game!'
+  if (mode == 'set the mode' || input == 'normal' || input == 'reverse' || input == 'muk-jji-ppa' || input == 'computer') {
+    let changeMode = setMode(input);
+    mode = changeMode;
+    if (mode == 'computer') {
+      return `${mode} version game mode is set. Press submit to play the game! (do not enter a choice)`;
+    }
+    return `${mode} version game mode is set. play the game!`;
   }
-  else if (input == 'normal') {
-    mode = 'normal';
-    return gameResult = 'normal mode set. play the game!'
-  }
-  else if (input == 'muk-jji-ppa') {
-    mode = 'muk-jji-ppa';
-    return gameResult = 'Korean version game mode is set. play the game!'
-  }
-  else if (input == 'computer') {
-    mode = 'computer';
-    return gameResult = 'computer vs computer version game mode is set. play the game!'
-  }
-
-  console.log(mode);
-
   // play the game
-  let randomItem = randomOption();
+  let randomItem = generateRandomOption();
   console.log(`random chosen item: ${randomItem}`);
-
-  if (mode == 'normal') {
-    if ((input !== 'scissors') && (input !== 'paper') && (input !== 'stone')) {
-      gameResult = `Oopppss ${input} is NOT AN OPTION. </br> <p>Please enter Scissors, Paper, or Stone to play the game.</p>`;
-      return gameResult;
-    }
-    gameResult = playNormal(randomItem, input);
-
+  if (input == '' && mode == 'computer') {
+    let computerRandom1 = generateRandomOption();
+    let computerRandom2 = generateRandomOption();
+    gameResult = playNormal(computerRandom1, computerRandom2);
   }
-
+  else if ((input !== 'scissors') && (input !== 'paper') && (input !== 'stone')) {
+    return `Oopppss ${input} is NOT AN OPTION. </br> <p>Please enter scissors, paper, or stone to play the game.</p>`;
+  }
+  if (mode == 'normal') {
+    gameResult = playNormal(randomItem, input);
+  }
   else if (mode == 'reverse') {
-    if ((input !== 'scissors') && (input !== 'paper') && (input !== 'stone')) {
-      gameResult = `Oopppss ${input} is NOT AN OPTION. </br> <p>Please enter Scissors, Paper, or Stone to play the game.</p>`;
-      return gameResult;
-    }
     gameResult = playReverse(randomItem, input);
   }
   else if (mode == 'muk-jji-ppa') {
-    if ((input !== 'scissors') && (input !== 'paper') && (input !== 'stone')) {
-      gameResult = `Oopppss ${input} is NOT AN OPTION. </br> <p>Please enter Scissors, Paper, or Stone to play the game.</p>`;
-      return gameResult;
-    }
     gameResult = koreanVersion(randomItem, input);
   }
-  else if (mode == 'computer') {
-    if ((input !== 'scissors') && (input !== 'paper') && (input !== 'stone')) {
-      gameResult = `Oopppss ${input} is NOT AN OPTION. </br> <p>Please enter Scissors, Paper, or Stone to play the game.</p>`;
-      return gameResult;
-    }
-    let computerRandom1 = randomOption();
-    let computerRandom2 = randomOption();
-
-    gameResult = playNormal(computerRandom1, computerRandom2);
-  }
-
   return gameResult;
 };
