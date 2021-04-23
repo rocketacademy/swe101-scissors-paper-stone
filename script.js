8af6fa094cad21c6748462aac2cb7179cdad0130
Su Lin Yeo <sulin@stripe.com>
2021-04-23 20:30:21 +0800

    a working version of scissors paper stone game. Not pretty yet but there is validation.

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var myOutputValue = "Default output value";
+  //Only kickstart the game if the user's input has been validated to either be scissors, paper, or stone
+  if (input == "scissors" || input == "paper" || input == "stone") {
+    return (myOutputValue = playGame(input));
+  } else {
+    myOutputValue =
+      'Please input either "scissors", "paper", or "stone". There are no other acceptable inputs.';
+    return myOutputValue;
+  }
+};
+
+var computerChoiceGenerator = function () {
+  var randomOption = Math.floor(Math.random() * 3 + 1);
+  if (randomOption == 1) {
+    return "scissors";
+  } else if (randomOption == 2) {
+    return "paper";
+  } else if (randomOption == 3) {
+    return "stone";
+  }
+  return "something broke";
+};
+
+var playGame = function (input) {
+  var computerChoice = computerChoiceGenerator();
+  if (computerChoice == "scissors") {
+    if (input == "paper") {
+      return (myOutputValue = `You lose! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "scissors") {
+      return (myOutputValue = `You tie! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "stone") {
+      return (myOutputValue = `You win! The computer played ${computerChoice} and you played ${input}.`);
+    }
+
+    //Scenario where computer plays paper
+  } else if (computerChoice == "paper") {
+    if (input == "paper") {
+      return (myOutputValue = `You tie! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "scissors") {
+      return (myOutputValue = `You win! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "stone") {
+      return (myOutputValue = `You lose! The computer played ${computerChoice} and you played ${input}.`);
+    }
+
+    //Scenario where computer plays stone
+  } else if ((computerChoice = "stone")) {
+    if (input == "paper") {
+      return (myOutputValue = `You win! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "scissors") {
+      return (myOutputValue = `You lose! The computer played ${computerChoice} and you played ${input}.`);
+    } else if (input == "stone") {
+      return (myOutputValue = `You tie! The computer played ${computerChoice} and you played ${input}.`);
+    }
+  }
 };
