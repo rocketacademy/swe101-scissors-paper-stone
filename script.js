6e2b2ea127423a345d555fd33d0af3f4950b6ac3
Ennnm <jiaen.1sc4@gmail.com>
2021-04-24 15:44:06 +0800

    Scissors paper stone game with reversed level

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+var main = function (userChoice) {
+  var output = "lost👻";
+  var computerChoice = ComputerChoice();
+  var isValid = 0;
+  if (
+    !(
+      userChoice == "scissors" ||
+      userChoice == "paper" ||
+      userChoice == "stone" ||
+      userChoice == "reversed scissors" ||
+      userChoice == "reversed paper" ||
+      userChoice == "reversed stone"
+    )
+  ) {
+    return "this is not a valid input ";
+  }
+
+  if (computerChoice == userChoice) {
+    output = "draw😱";
+  } else if (computerChoice == "scissors" && userChoice == "stone") {
+    output = "win👾";
+  } else if (computerChoice == "stone" && userChoice == "paper") {
+    output = "win👾";
+  } else if (computerChoice == "paper" && userChoice == "scissors") {
+    output = "win👾";
+  }
+  //reversed modes
+  else if (computerChoice == "scissors" && userChoice == "reversed paper") {
+    output = "win🙃";
+  } else if (computerChoice == "stone" && userChoice == "reversed scissors") {
+    output = "win🙃";
+  } else if (computerChoice == "paper" && userChoice == "reversed stone") {
+    output = "win🙃";
+  }
+
+  return `You ${output}<br> with ${userChoice}<br> against ${computerChoice}`;
+};
+
+var ComputerChoice = function () {
+  var output = "scissors";
+  var randomNum = Math.random() * 3;
+  var randomInt = Math.floor(randomNum) + 1;
+
+  if (randomInt == 2) {
+    output = "paper";
+  } else if (randomInt == 3) {
+    output = "stone";
+  }
+  return output;
 };
