39598e9d68c991c87ef5f4abfd9204f568af27f2
nyxnyxnyxed <mwidjaja91@gmail.com>
2021-04-27 04:01:27 +0800

    added project 1.5 features such as modes, winpercent, username etc

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
-var main = function (input) {
-  var computerHand = randomSPS();
+var currentGameMode = "waiting for username";
 
-  var myOutputValue = `You chose ${input}, the computer chose ${computerHand}. It's a tie!`;
+var userName = "";
+var winCount = 0;
+var loseCount = 0;
+var drawCount = 0;
+var winPercent = 0;
+
+var main = function (input) {
+  if (currentGameMode == "waiting for username") {
+    userName = input;
+
+    currentGameMode = "sps";
+
+    return `Hello ${userName}, let's begin playing SPS!`;
+  }
+
+  if (input == "autoplay") {
+    input = randomSPS();
+  }
+
+  if (input == "sps") {
+    currentGameMode = "sps";
+
+    return `Hello ${userName}, let's begin playing SPS!`;
+  }
+
+  if (input == "reverse") {
+    currentGameMode = "reverse sps";
+
+    return `Hello ${userName}, let's begin playing reverse SPS!`;
+  }
+
+  var computerHand = randomSPS();
+  var gameResult = "draw";
+
+  // var myOutputValue = `You chose ${input}, the computer chose ${computerHand}. It's a tie!`;
 
   if (
     (input == "scissors" && computerHand == "stone") ||
     (input == "paper" && computerHand == "scissors") ||
     (input == "stone" && computerHand == "paper")
   ) {
-    myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You lose!`;
+    gameResult = "lose";
   }
 
   if (
@@ -16,37 +49,68 @@ var main = function (input) {
     (input == "paper" && computerHand == "stone") ||
     (input == "stone" && computerHand == "scissors")
   ) {
-    myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You win!`;
+    gameResult = "win";
   }
 
   if (
     !(
-      input == "scissors" ||
-      input == "paper" ||
-      input == "stone" ||
-      input == "reversed scissors" ||
-      input == "reversed paper" ||
-      input == "reversed stone"
+      (input == "scissors" || input == "paper" || input == "stone")
+      // input == "reversed scissors" ||
+      // input == "reversed paper" ||
+      // input == "reversed stone"
     )
   ) {
-    myOutputValue =
-      "Please select between scissors, reversed scissors, paper, reversed paper, stone, or reversed stone.";
+    return "Please select between scissors, paper or stone.";
+  }
+
+  if (input == computerHand) {
+    drawCount = drawCount + 1;
+
+    myOutputValue = `The computer chose ${computerHand}. <br> You chose ${input}. <br> <br> It's a draw!
+    Player ${userName} has a W-L-D record of ${winCount}/${loseCount}/${drawCount}, with a win percentage of ${winPercent}%.`;
+  }
+
+  // if (
+  //   (input == "reversed scissors" && computerHand == "paper") ||
+  //   (input == "reversed paper" && computerHand == "stone") ||
+  //   (input == "reversed stone" && computerHand == "scissors")
+  // ) {
+  //   myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You lose!`;
+  // }
+
+  // if (
+  //   (input == "reversed scissors" && computerHand == "stone") ||
+  //   (input == "reversed paper" && computerHand == "scissors") ||
+  //   (input == "reversed stone" && computerHand == "paper")
+  // ) {
+  //   myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You win!`;
+  // }
+
+  if (
+    (currentGameMode == "sps" && gameResult == "win") ||
+    (currentGameMode == "reverse sps" && gameResult == "lose")
+  ) {
+    winCount = winCount + 1;
+    winPercent = (winCount / (winCount + loseCount)) * 100;
+    myOutputValue = `The computer chose ${computerHand}. <br> You chose ${input}. <br> <br> You win!   
+    Player ${userName} has a W-L-D record of ${winCount}/${loseCount}/${drawCount}, with a win percentage of ${winPercent}%.`;
   }
 
   if (
-    (input == "reversed scissors" && computerHand == "paper") ||
-    (input == "reversed paper" && computerHand == "stone") ||
-    (input == "reversed stone" && computerHand == "scissors")
+    (currentGameMode == "sps" && gameResult == "lose") ||
+    (currentGameMode == "reverse sps" && gameResult == "win")
   ) {
-    myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You lose!`;
+    loseCount = loseCount + 1;
+    winPercent = (winCount / (winCount + loseCount)) * 100;
+    myOutputValue = `The computer chose ${computerHand}. <br> You chose ${input}. <br> <br> You lose!   
+    Player ${userName} has a W-L-D record of ${winCount}/${loseCount}/${drawCount}, with a win percentage of ${winPercent}%.`;
   }
 
-  if (
-    (input == "reversed scissors" && computerHand == "stone") ||
-    (input == "reversed paper" && computerHand == "scissors") ||
-    (input == "reversed stone" && computerHand == "paper")
-  ) {
-    myOutputValue = `You chose ${input}, the computer chose ${computerHand}. You win!`;
+  if (winPercent > 50) {
+    myOutputValue = myOutputValue + " Nice job!";
+  }
+  if (winPercent < 50) {
+    myOutputValue = myOutputValue + " You can do it!";
   }
 
   return myOutputValue;
