e2e16f82fdb07953df38480641b3d07783e01bb7
JayWongCodeStuff <justinwong8991@gmail.com>
2021-05-26 23:51:50 +0800

    Added linebreaks in output

--- script.js
+++ script.js
@@ -5,36 +5,60 @@ var main = function (input) {
   //1 is paper, 2 is rock, and 3 is scissors
   if (input == "scissors" && RandChoice == 1) {
     myOutputValue =
-      "The computer chose paper. You chose scissors and therefore you have won! You can type again to play again.";
+      "The computer chose paper." +
+      "<br>" +
+      "You chose scissors and therefore you have won!" +
+      "<br>" +
+      " You can type again to play again.";
   }
   if (input == "scissors" && RandChoice == 2) {
     myOutputValue =
-      "The computer chose rock. You chose scissors and therefore you have lost. :( Try again?";
+      "The computer chose rock." +
+      "<br>" +
+      "You chose scissors and therefore you have lost. :(" +
+      "<br>" +
+      "Try again?";
   }
   if (input == "scissors" && RandChoice == 3) {
-    myOutputValue = "You both have chosen scissors. Its a draw!";
+    myOutputValue = "You both have chosen scissors." + "<br>" + "Its a draw!";
   }
   if (input == "paper" && RandChoice == 2) {
     myOutputValue =
-      "The computer chose rock. You chose paper and therefore you have won! You can type again to play again.";
+      "The computer chose rock." +
+      "<br>" +
+      "You chose paper and therefore you have won!" +
+      "<br>" +
+      "You can type again to play again.";
   }
   if (input == "paper" && RandChoice == 3) {
     myOutputValue =
-      "The computer chose scissors. You chose paper and therefore you have lost. :( Try again?";
+      "The computer chose scissors." +
+      "<br>" +
+      "You chose paper and therefore you have lost. :(" +
+      "<br>" +
+      "Try again?";
   }
   if (input == "paper" && RandChoice == 1) {
-    myOutputValue = "You both have chosen paper. Its a draw!";
+    myOutputValue = "You both have chosen paper." + "<br>" + "Its a draw!";
   }
   if (input == "rock" && RandChoice == 3) {
     myOutputValue =
-      "The computer chose scissors. You chose rock and therefore you have won! You can type again to play again.";
+      "The computer chose scissors." +
+      "<br>" +
+      "You chose rock and therefore you have won!" +
+      "<br>" +
+      "You can type again to play again.";
   }
   if (input == "rock" && RandChoice == 2) {
-    myOutputValue = "You both have chosen rock. Its a draw!";
+    myOutputValue = "You both have chosen rock." + "<br>" + "Its a draw!";
   }
   if (input == "rock" && RandChoice == 1) {
     myOutputValue =
-      "The computer chose paper. You chose rock and therefore you have lost. :( Try again?";
+      "The computer chose paper." +
+      "<br>" +
+      "You chose rock and therefore you have lost. :(" +
+      "<br>" +
+      "Try again?";
   }
   return myOutputValue;
 };
