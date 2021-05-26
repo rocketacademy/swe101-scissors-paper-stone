20f500a78859f438fb228c54c3b50cafc17946ce
JayWongCodeStuff <justinwong8991@gmail.com>
2021-05-26 23:45:26 +0800

    Basic RPS game coded.

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var RandChoice = RandRoll();
+  console.log(RandChoice);
+  var myOutputValue = "Wrong input! Please only input Scissors, Rock or Paper.";
+  //1 is paper, 2 is rock, and 3 is scissors
+  if (input == "scissors" && RandChoice == 1) {
+    myOutputValue =
+      "The computer chose paper. You chose scissors and therefore you have won! You can type again to play again.";
+  }
+  if (input == "scissors" && RandChoice == 2) {
+    myOutputValue =
+      "The computer chose rock. You chose scissors and therefore you have lost. :( Try again?";
+  }
+  if (input == "scissors" && RandChoice == 3) {
+    myOutputValue = "You both have chosen scissors. Its a draw!";
+  }
+  if (input == "paper" && RandChoice == 2) {
+    myOutputValue =
+      "The computer chose rock. You chose paper and therefore you have won! You can type again to play again.";
+  }
+  if (input == "paper" && RandChoice == 3) {
+    myOutputValue =
+      "The computer chose scissors. You chose paper and therefore you have lost. :( Try again?";
+  }
+  if (input == "paper" && RandChoice == 1) {
+    myOutputValue = "You both have chosen paper. Its a draw!";
+  }
+  if (input == "rock" && RandChoice == 3) {
+    myOutputValue =
+      "The computer chose scissors. You chose rock and therefore you have won! You can type again to play again.";
+  }
+  if (input == "rock" && RandChoice == 2) {
+    myOutputValue = "You both have chosen rock. Its a draw!";
+  }
+  if (input == "rock" && RandChoice == 1) {
+    myOutputValue =
+      "The computer chose paper. You chose rock and therefore you have lost. :( Try again?";
+  }
   return myOutputValue;
+};
+
+var RandRoll = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  var RandNumber = randomInteger + 1;
+  return RandNumber;
 };
