f8a2e1091510343e4c6c8dc0b507279579661e19
JayWongCodeStuff <justinwong8991@gmail.com>
2021-06-01 22:29:22 +0800

    Refactored code to reduce (some) unnecessary lines of code

--- script.js
+++ script.js
@@ -1,68 +1,81 @@
-var compPaper = "paper";
-var compRock = "rock";
-var compScissors = "scissors";
+var compPaper = "The computer chose paper";
+var compRock = "The computer chose rock";
+var compScissors = "The computer chose scissors";
 var main = function (input) {
   var RandChoice = RandRoll();
   console.log(RandChoice);
-  var myOutputValue = "Wrong input! Please only input Scissors, Rock or Paper.";
-  //1 is paper, 2 is rock, and 3 is scissors
-  //I have set variables to the rolls by the function in order to improve readibility
-  if (input == "scissors" && RandChoice == compPaper) {
-    myOutputValue =
-      "The computer chose paper." +
-      "<br>" +
-      "You chose scissors and therefore you have won!" +
-      "<br>" +
-      " You can type again to play again.";
+  var myOutputValue = "Wrong input! Please only input scissors, rock or paper.";
+  if (input == "scissors") {
+    if (RandChoice == compPaper) {
+      myOutputValue =
+        compPaper +
+        "<br>" +
+        "You chose scissors and therefore you have won!" +
+        "<br>" +
+        " You can type again to play again.";
+    } else if (RandChoice == compRock) {
+      myOutputValue =
+        compRock +
+        "<br>" +
+        "You chose scissors and therefore you have lost. :(" +
+        "<br>" +
+        "Try again?";
+    } else {
+      myOutputValue =
+        "You both have chosen scissors." +
+        "<br>" +
+        "Its a draw!" +
+        "<br>" +
+        "Try again?";
+    }
   }
-  if (input == "scissors" && RandChoice == compRock) {
-    myOutputValue =
-      "The computer chose rock." +
-      "<br>" +
-      "You chose scissors and therefore you have lost. :(" +
-      "<br>" +
-      "Try again?";
+  if (input == "paper") {
+    if (RandChoice == compRock) {
+      myOutputValue =
+        compRock +
+        "<br>" +
+        "You chose paper and therefore you have won!" +
+        "<br>" +
+        "You can type again to play again.";
+    } else if (RandChoice == compScissors) {
+      myOutputValue =
+        compScissors +
+        "<br>" +
+        "You chose paper and therefore you have lost. :(" +
+        "<br>" +
+        "Try again?";
+    } else {
+      myOutputValue =
+        "You both have chosen paper." +
+        "<br>" +
+        "Its a draw!" +
+        "<br>" +
+        "Try again?";
+    }
   }
-  if (input == "scissors" && RandChoice == compScissors) {
-    myOutputValue = "You both have chosen scissors." + "<br>" + "Its a draw!";
-  }
-  if (input == "paper" && RandChoice == compRock) {
-    myOutputValue =
-      "The computer chose rock." +
-      "<br>" +
-      "You chose paper and therefore you have won!" +
-      "<br>" +
-      "You can type again to play again.";
-  }
-  if (input == "paper" && RandChoice == compScissors) {
-    myOutputValue =
-      "The computer chose scissors." +
-      "<br>" +
-      "You chose paper and therefore you have lost. :(" +
-      "<br>" +
-      "Try again?";
-  }
-  if (input == "paper" && RandChoice == compPaper) {
-    myOutputValue = "You both have chosen paper." + "<br>" + "Its a draw!";
-  }
-  if (input == "rock" && RandChoice == compScissors) {
-    myOutputValue =
-      "The computer chose scissors." +
-      "<br>" +
-      "You chose rock and therefore you have won!" +
-      "<br>" +
-      "You can type again to play again.";
-  }
-  if (input == "rock" && RandChoice == compRock) {
-    myOutputValue = "You both have chosen rock." + "<br>" + "Its a draw!";
-  }
-  if (input == "rock" && RandChoice == compPaper) {
-    myOutputValue =
-      "The computer chose paper." +
-      "<br>" +
-      "You chose rock and therefore you have lost. :(" +
-      "<br>" +
-      "Try again?";
+  if (input == "rock") {
+    if (RandChoice == compScissors) {
+      myOutputValue =
+        compScissors +
+        "<br>" +
+        "You chose rock and therefore you have won!" +
+        "<br>" +
+        "You can type again to play again.";
+    } else if (RandChoice == compPaper) {
+      myOutputValue =
+        compPaper +
+        "<br>" +
+        "You chose rock and therefore you have lost. :(" +
+        "<br>" +
+        "Try again?";
+    } else {
+      myOutputValue =
+        "You both have chosen rock." +
+        "<br>" +
+        "Its a draw!" +
+        "<br>" +
+        "Try again?";
+    }
   }
   return myOutputValue;
 };
@@ -72,6 +85,8 @@ var RandRoll = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
   var RandNumber = randomInteger + 1;
+  console.log(RandNumber);
+  //1 is paper, 2 is rock, and 3 is scissors
   if (RandNumber == 1) {
     compOutcome = compPaper;
   } else if (RandNumber == 2) {
