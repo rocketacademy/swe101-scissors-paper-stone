303cc0e773207ee50f77f48b08eb6132bc357750
terryyeng-ux <yengterry@gmail.com>
2021-06-03 21:14:22 +0800

    first draft

--- script.js
+++ script.js
@@ -9,20 +9,31 @@ var main = function (input) {
   // input = scissors, paper or stone
   // output = user won, program won or it's a draw
   var choiceNumber = randomChoice();
-  console.log(choiceNumber);
 
   var myOutputValue = "It's a draw!";
 
-  if (input != "Scissors" || input != "Paper" || input != "Stone") {
+  var inputNumber;
+
+  var choice;
+
+  if (
+    input != "Scissors" ||
+    input != "Paper" ||
+    input != "Stone" ||
+    input != "Reversed Scissors" ||
+    input != "Reversed Paper" ||
+    input != "Reversed Stone"
+  ) {
     myOutputValue =
-      "Error <br> <br> Please input 'Scissors' 'Paper' or 'Stone'";
+      "Error <br> <br> Please input 'Scissors' 'Paper' 'Stone' or <br> <br> For reverse game <br> <br> Please input 'Reversed Scissors' 'Reversed Paper' 'Reversed Stone";
   }
 
-  if (input == "Scissors") var inputNumber = 1;
+  // Scissors = 1, Paper = 2, Stone = 3
+  if (input == "Scissors") inputNumber = 1;
 
-  if (input == "Paper") var inputNumber = 2;
+  if (input == "Paper") inputNumber = 2;
 
-  if (input == "Stone") var inputNumber = 3;
+  if (input == "Stone") inputNumber = 3;
 
   if (choiceNumber == 1) choice = "Scissors";
 
@@ -52,6 +63,40 @@ var main = function (input) {
   ) {
     myOutputValue = "You Win!";
   }
+
+  // Reverse Mode
+  if (input == "Reversed Scissors") inputNumber = 1;
+
+  if ((input = "Reversed Paper")) inputNumber = 2;
+
+  if ((input = "Reversed Stone")) inputNumber = 3;
+
+  // scenarios when player loses to program
+  if (
+    (inputNumber == 1 && choiceNumber == 2) ||
+    (inputNumber == 2 && choiceNumber == 3) ||
+    (inputNumber == 3 && choiceNumber == 1)
+  ) {
+    myOutputValue =
+      "Wrong! The computer chose " +
+      choice +
+      " <br> <br> You chose " +
+      input +
+      " <br> <br> You lose! Bummer. <br> <br> Now you can type 'Scissors' 'Paper' or 'Stone' to play another round!";
+    console.log(myOutputValue);
+  }
+
+  // scenarios when player wins program
+  if (
+    (inputNumber == 1 && choiceNumber == 3) ||
+    (inputNumber == 2 && choiceNumber == 1) ||
+    (inputNumber == 3 && choiceNumber == 2)
+  ) {
+    myOutputValue = "You Win!";
+    console.log(myOutputValue);
+  }
+
+  console.log("Action thrown by program: " + choice);
 
   return myOutputValue;
 };
