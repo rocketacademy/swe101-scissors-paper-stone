560869bdb20722486adf3dd88cd847682b8ea02f
Ameeralih <ameerali.hassanali@gmail.com>
2021-06-04 20:53:54 +0800

    Added reverse mode

--- script.js
+++ script.js
@@ -16,56 +16,122 @@ var RandomSPS = function () {
   }
 };
 
+var reverseModeStatus = false;
+
 var main = function (input) {
   SPS = RandomSPS();
+  if (input == "Reverse") {
+    reverseModeStatus = true;
+    return "Secret Reverse Mode On!";
+  }
 
-  if (input != "Scissors" && input != "Paper" && input != "Stone") {
+  if (input == "Reverse Off") {
+    reverseModeStatus = false;
+    return "Secret Reverse Mode Off!";
+  }
+
+  if (
+    input != "Scissors" &&
+    input != "Paper" &&
+    input != "Stone" &&
+    input != "Reverse" &&
+    input != "Reverse Off"
+  ) {
     yourChoiceMsg = "Please input either Scissors, Paper, or Stone.";
     result = "You did not pick a valid option.";
   }
 
-  if (SPS == "Scissors") {
-    if (input == "Scissors") {
-      yourChoiceMsg = "You chose Scissors!";
-      result = "It is a draw!";
+  if (!reverseModeStatus) {
+    if (SPS == "Scissors") {
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "It is a draw!";
+      }
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "You lose!";
+      }
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "You win!";
+      }
     }
-    if (input == "Paper") {
-      yourChoiceMsg = "You chose Paper!";
-      result = "You lose!";
+
+    if (SPS == "Paper") {
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "It is a draw!";
+      }
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "You lose!";
+      }
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "You win!";
+      }
     }
-    if (input == "Stone") {
-      yourChoiceMsg = "You chose Stone!";
-      result = "You win!";
+
+    if (SPS == "Stone") {
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "It is a draw!";
+      }
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "You lose!";
+      }
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "You win!";
+      }
     }
   }
 
-  if (SPS == "Paper") {
-    if (input == "Paper") {
-      yourChoiceMsg = "You chose Paper!";
-      result = "It is a draw!";
+  if (reverseModeStatus) {
+    if (SPS == "Scissors") {
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "It is a draw!";
+      }
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "You lose!";
+      }
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "You win!";
+      }
     }
-    if (input == "Stone") {
-      yourChoiceMsg = "You chose Stone!";
-      result = "You lose!";
-    }
-    if (input == "Scissors") {
-      yourChoiceMsg = "You chose Scissors!";
-      result = "You win!";
-    }
-  }
 
-  if (SPS == "Stone") {
-    if (input == "Stone") {
-      yourChoiceMsg = "You chose Stone!";
-      result = "It is a draw!";
+    if (SPS == "Paper") {
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "It is a draw!";
+      }
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "You lose!";
+      }
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "You win!";
+      }
     }
-    if (input == "Scissors") {
-      yourChoiceMsg = "You chose Scissors!";
-      result = "You lose!";
-    }
-    if (input == "Paper") {
-      yourChoiceMsg = "You chose Paper!";
-      result = "You win!";
+
+    if (SPS == "Stone") {
+      if (input == "Stone") {
+        yourChoiceMsg = "You chose Stone!";
+        result = "It is a draw!";
+      }
+      if (input == "Paper") {
+        yourChoiceMsg = "You chose Paper!";
+        result = "You lose!";
+      }
+      if (input == "Scissors") {
+        yourChoiceMsg = "You chose Scissors!";
+        result = "You win!";
+      }
     }
   }
 
