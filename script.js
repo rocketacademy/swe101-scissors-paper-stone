487639ae5d04679d169a1e1a60416539aa9c7877
adzdam <adrianrdamian@gmail.com>
2021-06-05 01:14:30 +0800

    SPS More Comfortable

--- script.js
+++ script.js
@@ -1,10 +1,104 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
+var main = function (choice) {
+  compChoice = randomNumber()
+  playerChoice = userChoice(choice)
+//0 = scissors, , 1 = paper , 2 = stone
+  console.log(compChoice);
+  console.log(playerChoice);
+
+  var myOutputValue = 'Please type only the following: "scissors", "paper", "stone"';
+  //make default messages
+  var defaultWinMessage = 'You won! 💪💪💪 '
+  var defaultPlayAgainMessage = 'Have a go again? type "scissors", "paper", "stone" and click submit'
+  var defaultLoseMessage = 'You lose! 👎 '
+
+  if (playerChoice == compChoice){
+    var myOutputValue = `It's a draw! 😃 <br> ${defaultPlayAgainMessage}`
+  }
+  if (playerChoice == 0 && compChoice == 1){
+    var myOutputValue = `The computer chose 📄 <br> You chose ✂ <br><br> ${defaultWinMessage} <br><br>${defaultPlayAgainMessage}`;
+  }
+  if (playerChoice == 0 && compChoice == 2){
+    var myOutputValue = `The computer chose 🗿 <br> You chose ✂ <br><br> ${defaultLoseMessage} <br><br>${defaultPlayAgainMessage}`;
+  }
+  if (playerChoice == 1 && compChoice == 0 ){
+    var myOutputValue = `The computer chose ✂ <br> You chose 📄  <br><br> ${defaultLoseMessage} <br><br>${defaultPlayAgainMessage}`;
+  }
+  if (playerChoice == 1 && compChoice == 2 ){
+    var myOutputValue = `The computer chose 🗿 <br> You chose 📄  <br><br> ${defaultWinMessage} <br><br>${defaultPlayAgainMessage}`;
+  }
+  if (playerChoice == 2 && compChoice == 0 ){
+    var myOutputValue = `The computer chose ✂ <br> You chose 🗿 <br><br> ${defaultWinMessage}  <br><br>${defaultPlayAgainMessage}`;
+  }
+  if (playerChoice == 2 && compChoice == 1 ){
+    var myOutputValue = `The computer chose 📄  <br> You chose 🗿 <br><br> ${defaultLoseMessage} <br><br>${defaultPlayAgainMessage}`;  
+  }
+
+  //reverse default messages win to lose and vise versa, 3 = reversed scissors, 4 = reversed paper, 5= reversed stone
+  if (playerChoice == 3){
+    var playerChoice = 0
+    if (playerChoice == compChoice){
+      var myOutputValue = `It's a draw! 😃 <br> ${defaultPlayAgainMessage}`
+    }
+    if (playerChoice == 0 && compChoice == 1){
+      var myOutputValue = `The computer chose 📄 <br> You chose ✂ <br><br> ${defaultLoseMessage} <br><br>${defaultPlayAgainMessage}`;
+    }
+    if (playerChoice == 0 && compChoice == 2){
+      var myOutputValue = `The computer chose 🗿 <br> You chose ✂ <br><br> ${defaultWinMessage} <br><br>${defaultPlayAgainMessage}`;
+    }
+  }
+
+  if (playerChoice == 4){
+    var playerChoice = 1;
+    if (playerChoice == compChoice){
+      var myOutputValue = `It's a draw! 😃 <br> ${defaultPlayAgainMessage}`
+    }
+    if (playerChoice == 1 && compChoice == 0 ){
+      var myOutputValue = `The computer chose ✂ <br> You chose 📄  <br><br> ${defaultWinMessage} <br><br>${defaultPlayAgainMessage}`;
+    }
+    if (playerChoice == 1 && compChoice == 2 ){
+      var myOutputValue = `The computer chose 🗿 <br> You chose 📄  <br><br> ${defaultLoseMessage} <br><br>${defaultPlayAgainMessage}`;
+    }
+  }
+    
+  if (playerChoice == 5){
+    var playerChoice = 2;
+    if (playerChoice == compChoice){
+      var myOutputValue = `It's a draw! 😃 <br> ${defaultPlayAgainMessage}`
+    }
+    if (playerChoice == 2 && compChoice == 0 ){
+      var myOutputValue = `The computer chose ✂ <br> You chose 🗿 <br><br> ${defaultLoseMessage}  <br><br>${defaultPlayAgainMessage}`;
+    }
+    if (playerChoice == 2 && compChoice == 1){
+      var myOutputValue = `The computer chose 📄  <br> You chose 🗿 <br><br> ${defaultWinMessage} <br><br>${defaultPlayAgainMessage}`;  
+    }
+  }
   return myOutputValue;
 };
-
-
-var testing = function(){
-  console.log('test')
-
+//random number for the computer choice
+var randomNumber = function(){
+  var randomInt = Math.random() * 3
+  var randomNum = Math.floor(randomInt)
+  return randomNum
 }
+
+// assign user choice to number 0= scissors , 1 = paper , 2 = stone, reversed will be from 3 to 5
+var userChoice = function(userInput){
+  if (userInput == 'scissors'){
+    return 0
+  }
+  if (userInput =='paper'){
+    return 1
+  } 
+  if (userInput == 'stone'){
+    return 2
+  }
+  if (userInput == 'reversed scissors'){
+    return 3
+  }
+  if (userInput =='reversed paper'){
+    return 4
+  }
+  if (userInput == 'reversed stone'){
+  return 5
+  }
+};
