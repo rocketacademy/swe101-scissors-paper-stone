1a30a0bc1b12adde174096046b512081c7a2126e
Wen Yi <seewenyi@gmail.com>
2021-06-04 22:44:52 +0800

    Made the changes for the RSP game

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var output = computerChoice();
+  console.log("computerChoice");
+  console.log(output);
+
+  if (input == output) {
+    return "it's a tie!";
+  }
+  if (
+    (input == "Scissors" && output == "Rock") ||
+    (input == "Rock" && output == "Paper") ||
+    (input == "Paper" && output == "Scissors")
+  ) {
+    return "you lose!";
+  }
+
+  if (
+    (input == "Scissors" && output == "Paper") ||
+    (input == "Rock" && output == "Scissors") ||
+    (input == "Paper" && output == "Rock")
+  ) {
+    return "you win!";
+  }
+  return output;
+};
+
+var computerChoice = function () {
+  var randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice == 0) {
+    return "Scissors";
+  }
+  if (randomChoice == 1) {
+    return "Paper";
+  }
+  if (randomChoice == 2) {
+    return "Rock";
+  }
+  return randomChoice;
 };
