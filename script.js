0366b4f3de62975975be2a1f253ce7c8bd207fb2
KP <kp.sendto@gmail.com>
2021-06-05 10:52:56 +0800

    completed

--- script.js
+++ script.js
@@ -1,4 +1,50 @@
+var getComputerOutcome = function () {
+  // randomly assigns 1, 2, or 3 to variable 'randomNumber'. function returns one of these strings: 'scissors', 'paper', 'stone'.
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber == 1) {
+    return `scissors`;
+  }
+  if (randomNumber == 2) {
+    return `paper`;
+  }
+  if (randomNumber == 3) {
+    return `stone`;
+  }
+};
+
+var displayGameOutcome = function (computerOutcome, userInput) {
+  // outcome: draw
+  if (computerOutcome == userInput) {
+    return `It's a draw.`;
+  }
+  // outcome: user loses
+  if (
+    (computerOutcome == `scissors` && userInput == `paper`) ||
+    (computerOutcome == `stone` && userInput == `scissors`) ||
+    (computerOutcome == `paper` && userInput == `stone`)
+  ) {
+    return `You lose.`;
+  }
+  // outcome: user wins
+  if (
+    (computerOutcome == `paper` && userInput == `scissors`) ||
+    (computerOutcome == `scissors` && userInput == `stone`) ||
+    (computerOutcome == `stone` && userInput == `paper`)
+  ) {
+    return `You win.`;
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  computerOutcome = getComputerOutcome();
+  console.log(`You chose: ${input}`);
+  console.log(`The computer chose: ${computerOutcome}`);
+
+  if (input != `scissors` && input != `paper` && input != `stone`) {
+    return `There are only 3 options. Try again. `;
+  }
+
+  gameOutcome = displayGameOutcome(computerOutcome, input);
+  myOutputValue = `${gameOutcome} You chose ${input} and the computer chose ${computerOutcome}.`;
   return myOutputValue;
 };
