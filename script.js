076178ce955c2a3bfa9e6d71f9bfd8dcf33f8d2d
KP <kp.sendto@gmail.com>
2021-06-05 11:10:12 +0800

    completed

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
+var getComputerOutcome = function () {
+  // randomly assigns 1, 2, or 3 to variable 'randomNumber'. function returns one of these strings: 'scissors', 'paper', 'stone'.
+  var randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber == 1) {
+    return `scissors`;
+  }
+  if (randomNumber == 2) {
+    return `paper`;
+  }
+  if (randomNumber == 3) {
+    return `stone`;
+  }
+};
+
+var displayGameOutcome = function (computerOutcome, userInput) {
+  // outcome: draw
+  if (computerOutcome == userInput) {
+    return `It's a draw.`;
+  }
+  // outcome: user loses
+  if (
+    (computerOutcome == `scissors` && userInput == `paper`) ||
+    (computerOutcome == `stone` && userInput == `scissors`) ||
+    (computerOutcome == `paper` && userInput == `stone`)
+  ) {
+    return `You lose.`;
+  }
+  // outcome: user wins
+  if (
+    (computerOutcome == `paper` && userInput == `scissors`) ||
+    (computerOutcome == `scissors` && userInput == `stone`) ||
+    (computerOutcome == `stone` && userInput == `paper`)
+  ) {
+    return `You win.`;
+  }
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  computerOutcome = getComputerOutcome();
+  console.log(`You chose: ${input}`);
+  console.log(`The computer chose: ${computerOutcome}`);
+
+  if (input != `scissors` && input != `paper` && input != `stone`) {
+    return `There are only 3 options. Try again. `;
+  }
+
+  gameOutcome = displayGameOutcome(computerOutcome, input);
+
+  // putting emojis after 'scissors', 'paper', and 'stone
+  if (input == `scissors`) {
+    var input = `scissors ✂️`;
+  }
+
+  if (input == `paper`) {
+    var input = `paper 🧻`;
+  }
+
+  if (input == `stone`) {
+    var input = `stone 🗿`;
+  }
+
+  if (computerOutcome == `scissors`) {
+    var computerOutcome = `scissors ✂️`;
+  }
+
+  if (computerOutcome == `paper`) {
+    var computerOutcome = `paper 🧻`;
+  }
+
+  if (computerOutcome == `stone`) {
+    var computerOutcome = `stone 🗿`;
+  }
+
+  myOutputValue = `${gameOutcome} <br> You chose ${input} and the computer chose ${computerOutcome}. <br><br> Now you can type "scissors", "paper", or "stone" to play another round!`;
   return myOutputValue;
 };
