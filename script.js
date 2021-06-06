603116c9530cef2afe441a78d7ddd7dfb0a708f4
kangraye <kangraye.ng@gmail.com>
2021-06-07 01:01:06 +0800

    Part 2, unfactorized code

--- script.js
+++ script.js
@@ -5,55 +5,60 @@ var generateOutcome = function () {
   return outcome;
 };
 
-// Count wins and losses, but ignore invalid inputs
+// Create global variables
+var gameState = "NameInput";
+var playerName = "Player";
 var wins = 0;
 var loss = 0;
 var games = 0;
+var previousWinner = "no one";
+
+// create outcome variables
+var win = "Congratulations, you win! Want a rematch?";
+var lose = "Bummer, you lost. Try again next time!";
+var tie = "Its a draw. We'll have to go again";
+var invalid = `This is a game of scissors paper stone. Accepted input values are "scissors", "paper", "stone" or "random".`;
 
 var main = function (input) {
-  // create outcome variables
-  var win = "Congratulations, you win! Want a rematch?";
-  var lose = "Bummer, you lost. Try again next time!";
-  var tie = "Its a draw. We'll have to go again";
-  var invalid = `This is a game of scissors paper stone. Accepted input values are "scissors", "paper" or "stone".`;
-
-  // check input and outputs
-  var computerOutcome = generateOutcome();
-  console.log("Input: " + input);
-  console.log("Generated: " + computerOutcome);
-
-  // check for reversed in two string word
-  var wordsInput = input.split(" ");
-  var inputLength = wordsInput.length;
-  var outcome = invalid;
-  if (inputLength == 2 && wordsInput[0] == "reversed") {
-    // play reversed scissors paper stone
-    var userChoice = wordsInput[1];
+  if (gameState == "NameInput") {
+    // Start getting user input
+    playerName = input;
+    var myOutputValue = `Welcome to play the game ${playerName}. <br> Please select the following game modes: "Normal", "Reversed" or "Korean".`;
+    gameState = "GameSelect";
+    return myOutputValue;
+  } else if (gameState == "GameSelect") {
+    // Reset counters for existing game mode.
+    wins = 0;
+    loss = 0;
+    games = 0;
+    var gameSelect = input;
     if (
-      (userChoice == "scissors" && computerOutcome == "stone") ||
-      (userChoice == "paper" && computerOutcome == "scissors") ||
-      (userChoice == "stone" && computerOutcome == "paper")
+      gameSelect == "Normal" ||
+      gameSelect == "Reversed" ||
+      gameSelect == "Korean"
     ) {
-      outcome = win;
-      wins = wins + 1;
-      games = games + 1;
-    } else if (userChoice == computerOutcome) {
-      outcome = tie;
-      games = games + 1;
-    } else if (
-      (userChoice == "scissors" && computerOutcome == "paper") ||
-      (userChoice == "paper" && computerOutcome == "stone") ||
-      (userChoice == "stone" && computerOutcome == "scissors")
-    ) {
-      outcome = lose;
-      loss = loss + 1;
-      games = games + 1;
+      gameState = gameSelect;
+      myOutputValue = `Good choice ${playerName}! We will now proceed to play ${gameSelect} version of scissors, paper, stone! Best of Luck!`;
     } else {
-      outcome = invalid;
+      myOutputValue = `Sorry ${playerName}, this is an invalid entry. <br> Please select the following game modes: "Normal", "Reversed" or "Korean".`;
     }
-  } else {
-    // standard game
-    userChoice = input;
+    return myOutputValue;
+  } else if (gameState == "Normal") {
+    // check user and computer inputs
+    if (input == "random") {
+      var userChoice = generateOutcome();
+    } else {
+      var userChoice = input;
+    }
+
+    var computerOutcome = generateOutcome();
+    console.log("Input: " + userChoice);
+    console.log("Generated: " + computerOutcome);
+
+    // the default outcome of all games shall be invalid
+    var outcome = invalid;
+
+    // If else logic for normal mode
     if (
       (userChoice == "scissors" && computerOutcome == "stone") ||
       (userChoice == "paper" && computerOutcome == "scissors") ||
@@ -76,11 +81,108 @@ var main = function (input) {
     } else {
       outcome = invalid;
     }
+  } else if (gameState == "Reversed") {
+    // check user and computer inputs
+    if (input == "random") {
+      var userChoice = generateOutcome();
+    } else {
+      var userChoice = input;
+    }
+    var computerOutcome = generateOutcome();
+    console.log("Input: " + userChoice);
+    console.log("Generated: " + computerOutcome);
+
+    // the default outcome of all games shall be invalid
+    var outcome = invalid;
+
+    if (
+      (userChoice == "scissors" && computerOutcome == "stone") ||
+      (userChoice == "paper" && computerOutcome == "scissors") ||
+      (userChoice == "stone" && computerOutcome == "paper")
+    ) {
+      outcome = win;
+      wins = wins + 1;
+      games = games + 1;
+    } else if (userChoice == computerOutcome) {
+      outcome = tie;
+      games = games + 1;
+    } else if (
+      (userChoice == "scissors" && computerOutcome == "paper") ||
+      (userChoice == "paper" && computerOutcome == "stone") ||
+      (userChoice == "stone" && computerOutcome == "scissors")
+    ) {
+      outcome = lose;
+      loss = loss + 1;
+      games = games + 1;
+    } else {
+      outcome = invalid;
+    }
+  } else if (gameState == "Korean") {
+    // check user and computer inputs
+    if (input == "random") {
+      var userChoice = generateOutcome();
+    } else {
+      var userChoice = input;
+    }
+    var computerOutcome = generateOutcome();
+    console.log("Input: " + userChoice);
+    console.log("Generated: " + computerOutcome);
+
+    // the default outcome of all games shall be invalid
+    var outcome = invalid;
+
+    // If else logic for korean mode
+    if (
+      (userChoice == "scissors" && computerOutcome == "stone") ||
+      (userChoice == "paper" && computerOutcome == "scissors") ||
+      (userChoice == "stone" && computerOutcome == "paper")
+    ) {
+      previousWinner = "computer";
+      outcome =
+        "A draw in the next round would see the computer win this version of scissors, paper, stone.";
+    } else if (userChoice == computerOutcome) {
+      if (previousWinner == "computer") {
+        outcome = lose;
+        games = games + 1;
+        loss = loss + 1;
+      } else if (previousWinner == playerName) {
+        outcome = win;
+        games = games + 1;
+        wins = wins + 1;
+      } else {
+        outcome = tie;
+      }
+    } else if (
+      (userChoice == "scissors" && computerOutcome == "paper") ||
+      (userChoice == "paper" && computerOutcome == "stone") ||
+      (userChoice == "stone" && computerOutcome == "scissors")
+    ) {
+      previousWinner = playerName;
+      outcome =
+        "A draw in the next round allow you to win this version of scissors, paper, stone.";
+    } else {
+      outcome = invalid;
+    }
+    console.log("PreviousWinner:", previousWinner);
+    console.log("Player Name:" + playerName);
+    console.log("Boolean Check: " + previousWinner == playerName);
   }
+
+  // Check current performance
+  var performance = wins / games;
+  if (performance >= 0.75) {
+    var evaluation = "Very impressive!";
+  } else if (performance <= 0.25) {
+    var evaluation = "Try harder!";
+  } else {
+    var evaluation = "Keep going!";
+  }
+
+  // Print output of game outcome
   myOutputValue =
-    `You chose ${input}. <br>The computer generated ${computerOutcome}<br><br>` +
+    `You chose ${userChoice}. <br>The computer generated ${computerOutcome}<br><br>` +
     outcome +
-    `<br><br>Your current record is as follows<br>
+    `<br><br>So far ${playerName}, your record is as follows. ${evaluation}<br>
     Games: ${games}<br>
     Wins: ${wins}<br>
     Loss: ${loss}<br>
