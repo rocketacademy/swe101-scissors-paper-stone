2d6eef164b5a39d9dab789fec2d29c322c619d2e
gw98 <glenwee1998@gmail.com>
2021-06-07 21:28:02 +0800

    added korean version of game

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ var numWins = 0;
 var winPercent = "";
 var winRecord = "";
 var userName = "";
-var gameMode = `waiting for username`;
+var gameMode = "waiting for username";
+var mostRecentWinner = "";
 
 var main = function (input) {
   if (gameMode == "waiting for username") {
@@ -23,6 +24,9 @@ var main = function (input) {
     if (gameMode == "reverse") {
       return reversedSPSGame(userName, input);
     }
+    if (gameMode == "korean") {
+      return koreanSPSGame(userName, input);
+    }
   }
 };
 
@@ -31,6 +35,9 @@ var spsGame = function (userName, input) {
   if (input == "reverse") {
     gameMode = "reverse";
     return `The rules of the SPS game have been reversed`;
+  } else if (input == "korean") {
+    gameMode = "korean";
+    return `The Korean SPS game will start now.`;
   }
 
   var comOutput = generateComOutput();
@@ -79,7 +86,7 @@ var spsGame = function (userName, input) {
   }
 };
 
-// reversed SPS game
+// reversed SPS game: rules reversed
 var reversedSPSGame = function (userName, input) {
   var comOutput = generateComOutput();
   var standardMessage = `${userName}, you chose ${input} <br> The computer chose ${comOutput} <br>`;
@@ -132,12 +139,61 @@ var reversedSPSGame = function (userName, input) {
   }
 };
 
+// korean SPS game: records most recent winner
+var koreanSPSGame = function (userName, input) {
+  var comOutput = generateComOutput();
+  var standardMessage = `${userName}, you chose ${input} <br> The computer chose ${comOutput} <br>`;
+  var endMessage = `Now you can type "scissors", "paper" or "stone" to play another round!`;
+  numRolls = numRolls + 1;
+
+  //Computer and User draw when they choose the same object
+  if (input == comOutput) {
+    winRecord = `${numWins} out of ${numRolls}`;
+    var ProgressMessage = generateProgressMessage(numWins, numRolls);
+
+    return `${standardMessage} <br> You draw! <br> <br> So far ${userName}, you've been winning ${winRecord} turns. <br> <br> ${mostRecentWinner} is thus the ultimate winner!`;
+  }
+
+  //scissors beats paper, paper beats stone, and stone beats scissors.
+
+  //Computer wins and user loses
+  if (
+    (comOutput == SCISSORS && input == PAPER) ||
+    (comOutput == PAPER && input == STONE) ||
+    (comOutput == STONE && input == SCISSORS)
+  ) {
+    winRecord = `${numWins} out of ${numRolls}`;
+    var ProgressMessage = generateProgressMessage(numWins, numRolls);
+    mostRecentWinner = "Computer";
+
+    return `${standardMessage} <br> You lost! Bummer! :( <br> <br> So far ${userName}, you've been winning ${winRecord} turns. <br> <br> ${endMessage}  ${ProgressMessage}`;
+  }
+
+  //Computer loses and user wins
+  if (
+    (input == SCISSORS && comOutput == PAPER) ||
+    (input == PAPER && comOutput == STONE) ||
+    (input == STONE && comOutput == SCISSORS)
+  ) {
+    numWins = numWins + 1;
+    winRecord = `${numWins} out of ${numRolls}`;
+    var ProgressMessage = generateProgressMessage(numWins, numRolls);
+    mostRecentWinner = userName;
+
+    return `${standardMessage} <br> You won!!! Hurray! :) <br> <br> So far ${userName}, you've been winning ${winRecord} turns. <br> <br> ${endMessage} ${ProgressMessage}`;
+  }
+
+  //Input validation
+  if (!(input == SCISSORS || input == PAPER || input == STONE)) {
+    return `There are only 3 input options: "scissors", "paper" or "stone". <br> Please try again with any of the three options.`;
+  }
+};
+
 //Generate a random integer from 1 to 3 inclusive; 3 because only scissors, paper or stone
 var generateComOutput = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal) + 1;
   console.log(randomInteger);
-  return SCISSORS;
   if (randomInteger == 1) {
     return SCISSORS;
   }
