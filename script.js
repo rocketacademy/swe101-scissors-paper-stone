af7012ee2b55e8aa31df47f168e1f29610ad54c1
veritableanon <contact.vernontai@gmail.com>
2021-06-07 15:38:22 +0800

    win-loss-record

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//set global variable for number of times computer won
+var numComputerWins = 0;
+
+//set global var for number of times user won
+var numUserWins = 0;
+
+//set global var for number of times user draws
+var numUserDraws = 0;
+
 var SCISSORS = "scissors";
 var PAPER = "paper";
 var STONE = "stone";
@@ -6,6 +15,9 @@ var REVERSED_SCISSORS = "reversed scissors";
 var REVERSED_PAPER = "reversed paper";
 var REVERSED_STONE = "reversed stone";
 
+/* ================================================================= HELPER FUNCTION ==============
+===============================================
+ */
 //program to choose scissors, paper or stone randomly
 var chooseSPS = function () {
   var randomDecimal = Math.random() * 3;
@@ -27,9 +39,16 @@ var chooseSPS = function () {
 
 //Compare computer answer with user input
 var determineWinner = function (userInput, computerObject) {
-  if (userInput == computerObject) {
+  // determine draw when user's input or reversed input same as computerObject
+  if (
+    userInput == computerObject ||
+    (userInput == "reversed scissors" && computerObject == SCISSORS) ||
+    (userInput == "reversed paper" && computerObject == PAPER) ||
+    (userInput == "reversed stone" && computerObject == STONE)
+  ) {
     return "It's a draw";
   }
+  // determine if player has won using regular SPS and reverse SPS
   if (
     (userInput == SCISSORS && computerObject == PAPER) ||
     (userInput == PAPER && computerObject == STONE) ||
@@ -54,29 +73,47 @@ var getObjectIcon = function (object) {
     return "💎";
   }
 };
-
+/* =================================================================MAIN================================================================
+ */
 var main = function (input) {
-  //set a var that holds the user's input
-  var userObject = input;
+  // validate input
   if (
-    userObject != SCISSORS &&
-    userObject != PAPER &&
-    userObject != STONE &&
-    userObject != REVERSED_SCISSORS &&
-    userObject != REVERSED_PAPER &&
-    userObject != REVERSED_STONE
+    input != SCISSORS &&
+    input != PAPER &&
+    input != STONE &&
+    input != REVERSED_SCISSORS &&
+    input != REVERSED_PAPER &&
+    input != REVERSED_STONE
   ) {
     return "Please key in scissors, paper or stone only";
   }
+
+  //set a var that holds the user's input
+  var userObject = input;
+
   //get the computer's object
   var computerObject = chooseSPS();
   // compare objects and obtain a winner
   var winner = determineWinner(userObject, computerObject);
+  if (winner == "User wins") {
+    //count number of user wins
+    numUserWins = numUserWins + 1;
+  } else if (winner == "User loses") {
+    //count number of computer wins
+    numComputerWins = numComputerWins + 1;
+  } else {
+    //count number of draws
+    numUserDraws = numUserDraws + 1;
+  }
+  //get matching icon for user's and computer's object
   var userObjectIcon = getObjectIcon(userObject);
   var computerObjectIcon = getObjectIcon(computerObject);
+  // return output to user
   return `${winner}! <br>
   You chose ${userObject} ${userObjectIcon}. <br>
-  The computer chose ${computerObject} ${computerObjectIcon}. <br>
+  The computer chose ${computerObject} ${computerObjectIcon}. <br> 
+  Wins: ${numUserWins}, Losses: ${numComputerWins}, Draws: ${numUserDraws}
+  <br>
   Now you can type 'scissors', 'paper' or 'stone' to play another round!`;
 };
 
