72aa87b21eef8681a4b7fc3f5653b0790f0737f1
veritableanon <contact.vernontai@gmail.com>
2021-06-07 17:07:10 +0800

    added reverse-game-mode feature

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//set default game mode
+var REGULAR_MODE = "regular";
+var REVERSE_MODE = "reverse";
+var gameMode = "";
+var modeSelectionInstructions = `Please choose the game mode: <br>
+  1. ${REGULAR_MODE} <br> 2. ${REVERSE_MODE}`;
+
 //set global variable for userName
 var userName = "";
 
@@ -41,21 +48,33 @@ var chooseSPS = function () {
 };
 
 //Compare computer answer with user input
-var determineWinner = function (userInput, computerObject) {
-  // determine draw when user's input or reversed input same as computerObject
+var determineRegularWinner = function (userInput, computerObject) {
+  // determine draw when user's input same as computerObject
+  if (userInput == computerObject) {
+    return "It's a draw";
+  }
+  // determine if player has won using regular SPS
   if (
-    userInput == computerObject ||
+    (userInput == SCISSORS && computerObject == PAPER) ||
+    (userInput == PAPER && computerObject == STONE) ||
+    (userInput == STONE && computerObject == SCISSORS)
+  ) {
+    return "User wins";
+  }
+  return "User loses";
+};
+
+var determineReverseWinner = function (userInput, computerObject) {
+  // determine draw when user's reversed input same as computerObject
+  if (
     (userInput == "reversed scissors" && computerObject == SCISSORS) ||
     (userInput == "reversed paper" && computerObject == PAPER) ||
     (userInput == "reversed stone" && computerObject == STONE)
   ) {
     return "It's a draw";
   }
-  // determine if player has won using regular SPS and reverse SPS
+  // determine if player has won using reverse SPS
   if (
-    (userInput == SCISSORS && computerObject == PAPER) ||
-    (userInput == PAPER && computerObject == STONE) ||
-    (userInput == STONE && computerObject == SCISSORS) ||
     (userInput == REVERSED_SCISSORS && computerObject == STONE) ||
     (userInput == REVERSED_PAPER && computerObject == SCISSORS) ||
     (userInput == REVERSED_STONE && computerObject == PAPER)
@@ -76,6 +95,14 @@ var getObjectIcon = function (object) {
     return "💎";
   }
 };
+
+var getInstructions = function () {
+  if (gameMode == REGULAR_MODE) {
+    return "Enter scissors, paper or stone to play.";
+  } else if (gameMode == REVERSE_MODE) {
+    return "Enter reversed scisssors, reversed paper or reversed stone to play.";
+  }
+};
 /* =================================================================MAIN================================================================
  */
 var main = function (input) {
@@ -84,56 +111,90 @@ var main = function (input) {
     return (
       "Welcome " +
       userName +
-      " to the scissors paper stone game. Please key in scissors, paper or stone to play."
+      " to the scissors paper stone game. " +
+      modeSelectionInstructions
     );
   }
-  // validate input
-  if (
-    input != SCISSORS &&
-    input != PAPER &&
-    input != STONE &&
-    input != REVERSED_SCISSORS &&
-    input != REVERSED_PAPER &&
-    input != REVERSED_STONE
-  ) {
-    return "Please key in scissors, paper or stone only";
+  //get user to choose a game mode
+  if (gameMode == "") {
+    gameMode = input;
+    var instructions = getInstructions();
+    return "You have chosen " + gameMode + ". " + instructions;
   }
 
-  //set a var that holds the user's input
-  var userObject = input;
+  if (gameMode == REGULAR_MODE) {
+    // validate input
+    if (input != SCISSORS && input != PAPER && input != STONE) {
+      return "Please key in scissors, paper or stone only";
+    }
+    //set a var that holds the user's input
+    var userObject = input;
 
-  //get the computer's object
-  var computerObject = chooseSPS();
-  // compare objects and obtain a winner
-  var winner = determineWinner(userObject, computerObject);
-  if (winner == "User wins") {
-    //count number of user wins
-    numUserWins = numUserWins + 1;
-  } else if (winner == "User loses") {
-    //count number of computer wins
-    numComputerWins = numComputerWins + 1;
-  } else {
-    //count number of draws
-    numUserDraws = numUserDraws + 1;
+    //get the computer's object
+    var computerObject = chooseSPS();
+    // compare objects and obtain a winner
+    var winner = determineRegularWinner(userObject, computerObject);
+    if (winner == "User wins") {
+      //count number of user wins
+      numUserWins = numUserWins + 1;
+    } else if (winner == "User loses") {
+      //count number of computer wins
+      numComputerWins = numComputerWins + 1;
+    } else {
+      //count number of draws
+      numUserDraws = numUserDraws + 1;
+    }
+    //get matching icon for user's and computer's object
+    var userObjectIcon = getObjectIcon(userObject);
+    var computerObjectIcon = getObjectIcon(computerObject);
+    //let user reselect game mode at the end of each round
+    gameMode = "";
+    // return output to user
+    return `${winner}! <br>
+  You chose ${userObject} ${userObjectIcon}. <br>
+  The computer chose ${computerObject} ${computerObjectIcon}. <br> 
+  ${userName}'s wins: ${numUserWins}, Losses: ${numComputerWins}, Draws: ${numUserDraws}
+  <br> ${modeSelectionInstructions}`;
   }
-  //get matching icon for user's and computer's object
-  var userObjectIcon = getObjectIcon(userObject);
-  var computerObjectIcon = getObjectIcon(computerObject);
-  // return output to user
-  return `${winner}! <br>
+
+  if (gameMode == REVERSE_MODE) {
+    // validate input
+    if (
+      input != REVERSED_SCISSORS &&
+      input != REVERSED_PAPER &&
+      input != REVERSED_STONE
+    ) {
+      return "Please key in reversed scissors, reversed paper or reversed stone only";
+    }
+    //set a var that holds the user's input
+    var userObject = input;
+
+    //get the computer's object
+    var computerObject = chooseSPS();
+    // compare objects and obtain a winner
+    var winner = determineReverseWinner(userObject, computerObject);
+    if (winner == "User wins") {
+      //count number of user wins
+      numUserWins = numUserWins + 1;
+    } else if (winner == "User loses") {
+      //count number of computer wins
+      numComputerWins = numComputerWins + 1;
+    } else {
+      //count number of draws
+      numUserDraws = numUserDraws + 1;
+    }
+    //get matching icon for user's and computer's object
+    var userObjectIcon = getObjectIcon(userObject);
+    var computerObjectIcon = getObjectIcon(computerObject);
+
+    //let user reselect game mode at the end of each round
+    gameMode = "";
+    // return output to user
+    return `${winner}! <br>
   You chose ${userObject} ${userObjectIcon}. <br>
   The computer chose ${computerObject} ${computerObjectIcon}. <br> 
   ${userName}'s wins: ${numUserWins}, Losses: ${numComputerWins}, Draws: ${numUserDraws}
   <br>
-  Now you can type 'scissors', 'paper' or 'stone' to play another round!`;
+  ${modeSelectionInstructions}`;
+  }
 };
-
-//user inputs "scissors" or "paper" or "stone"
-// under var main function, user's input (scissors or paper or stone) can call upon 3 other vars/functions
-
-//program outputs whether the user won, the program won, or draw.
-
-//Rule 1: scissors beats paper
-//Rule 2: paper beats stone
-//Rule 3: stone beats scissors
-//Rule 4: If both parties choose the same object, it's a draw.
