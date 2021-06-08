c15eea9019d2ecd992ee03e0ed4c310927c91405
janicecjy <janicechew2906@gmail.com>
2021-06-08 20:57:09 +0800

    updated SPS

--- script.js
+++ script.js
@@ -12,29 +12,6 @@ var main = function (input) {
   var paper = "paper";
   var stone = "stone";
 
-  // if (!userName) {
-  //   if (input) {
-  //     return "Please type in your username!";
-  //   }
-  //   userName = input;
-  //   return (
-  //     "Thank you " +
-  //     userName +
-  //     " To start playing SPS, please type in scissors, paper, or stone."
-  //   );
-  // }
-
-  // if (input != userName) {
-  //   return "Please enter your username before attempting the game.";
-  // } else if (currentGameMode == "waiting for user name") {
-  //   userName = input;
-  //   currentGameMode = "sps game";
-  //   myOutputValue =
-  //     "Welcome " +
-  //     userName +
-  //     "! Let us begin. Please type scissors, paper or stone to play";
-  // }
-
   if (input == "") {
     return "Please enter your username before attempting the game.";
   }
@@ -52,11 +29,6 @@ var main = function (input) {
   if (input != paper && input != stone && input != scissors) {
     return myOutputValue;
   }
-
-  // UserNumOfCorrectGuess += 1;
-  // console.log(UserNumOfCorrectGuess);
-  // CompNumOfCorrectGuess += 1;
-  // console.log(CompNumOfCorrectGuess);
 
   if (randomObject == 0) {
     console.log(scissors);
@@ -206,14 +178,3 @@ var GenerateRandomHand = function () {
   var randomHand = Math.floor(randomNumber);
   return randomHand;
 };
-
-//if user inputs 'scissors' or 'paper' or 'stone', computer randomly chooses 'scissors' or 'paper' or 'stone'.
-//and the computer outputs whether the user won, the computer won, or a draw.
-//Rule: scissors beat paper, paper beats stone, stone beats scissors. If both parties choose same, draw.
-//Input Validation----Sometimes the user types bad and types something other than "scissors", "paper", or "stone" during gameplay. Add input validation to kindly let the user know that there are only 3 input options, and ask them to try again.
-
-// Add state to keep track of the number of times the user has won and the number of times the computer has won.
-// Output: Win Loss Draw situation, and/or each party's winning %.
-
-// Add a feature to collect the user's name as the first input after the page loads. We can prompt the user to enter their name first by adding to the page's HTML.
-// Once the user submits their name, the program can return output to prompt the user to start playing Scissors Paper Stone by entering one of the 3 objects. Use the user's name to personalise win-loss record and other relevant output.
