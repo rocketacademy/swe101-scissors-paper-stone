30c369ee5954bb417b9e57a55ad6dc9276cb0bde
Zalskee <faizalnooraznan@gmail.com>
2021-06-08 18:58:31 +0800

    Part 2 of Project - need to debug cos program doesn't work

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
+var userName = '';
+var currentGameMode = 'waiting for user name';
+
 var ROCK = 'rock'
 var PAPER = 'paper';
 var SCISSORS = 'scissors';
 
-
-//To do: need to check how to print the number of wins (sth to do with global variables)
-// var numPlayerWins = 0;
-// var numComputerWins = 0;
-// var numDraws = 0;
+var numPlayerWins = 0;
+var numComputerWins = 0;
+var numDraws = 0;
+var numTotalRounds = 0;
 
 
 
 var main = function (input) {
-  var playerChoice = input;
-   var computerChoice = getComputerChoice();
-   var myOutputValue = 'You lost! You chose: ' +input+  '<br> The computer chose: ' +computerChoice;
-   console.log('Your choice')
-   console.log(input)
-   console.log('Computer choice')
-   console.log(computerChoice)
-  if (input != (ROCK || PAPER || SCISSORS)) {
-    return 'Please input "rock", "paper" or "scissors" only'
+
+  var  myOutputValue = '';
+  if (currentGameMode == 'waiting for user name')  {
+    userName = input;
+    // now that we have the name, switch the mode
+    currentGameMode = 'RPS game';
+    myOutputValue = `Hello ${userName}, let's play rock-paper-scissors!`    
+  
+    //only run the next block of code if we are in dice game mode
+  } else if ( currentGameMode == 'RPS game' ) { 
+    myOutputValue = playRPSGame(userName, input);
   }
-  if (playerChoice == computerChoice) {
-    numDraws = numDraws +1;
-    myOutputValue = 'It is a draw!' 
-  }
-  if (didPlayerBeatComputer(playerChoice, computerChoice)){
-    numPlayerWins = numPlayerWins + 1;
-    myOutputValue = 'You won! You chose: ' +input+ '<br> The computer chose: ' +computerChoice
-  }
-  numComputerWins = numComputerWins + 1;
   return myOutputValue;
+}
+ 
+
+var playRPSGame = function(userName, userChoice) {
+  var message = '';
+   
+  // first, check if userChoice is not Rock, Paper or Scissors
+  if (userChoice != (ROCK || PAPER || SCISSORS)) {
+    message = `Please input "rock", "paper" or "scissors" only`
+    return message
+  }
+
+   var computerChoice = getComputerChoice();
+   console.log('Your hand')
+   console.log(userChoice)
+   console.log('Computer hand')
+   console.log(computerChoice)
+
+   var winPercentage = calcWinPercentage(numPlayerWins, numTotalRounds)
+   console.log('win percentage')
+   console.log(winPercentage)
+  
+  if (userChoice == computerChoice) {
+    numDraws = numDraws +1;
+    numTotalRounds = numTotalRounds +1;
+    message = `${userName} It is a draw! <br><br> You played: ${userChoice} <br><br> The computer played: ${computerChoice}. <br><br> Total rounds: ${numTotalRounds}. <br> ${userName}'s win rate: ${winPercentage}`
+    return message
+  }
+
+  if (didPlayerBeatComputer(userChoice, computerChoice)){
+    numPlayerWins = numPlayerWins + 1;
+    numTotalRounds = numTotalRounds +1;
+    message = `${userName}, you won! <br><br> You played: ${userChoice} <br><br> The computer played: ${computerChoice}. <br><br> Total rounds: ${numTotalRounds}. <br> ${userName}'s win rate: ${winPercentage}`
+    return message
+  }
+
+  if (!didPlayerBeatComputer(userChoice, computerChoice)){
+    numComputerWins = numComputerWins + 1;
+    numTotalRounds = numTotalRounds +1;
+    message = `${userName}, you lost! <br><br> You played: ${userChoice} <br><br> The computer played: ${computerChoice}. <br><br> Total rounds: ${numTotalRounds}. <br> ${userName}'s win rate: ${winPercentage}`
+    return message
+  }
  };
+
+
 
 
 var getComputerChoice = function() {
@@ -51,3 +90,7 @@ var didPlayerBeatComputer = function(playerChoice, computerChoice) {
   (playerChoice == PAPER && computerChoice == ROCK)
 };
 
+
+var calcWinPercentage = function(wins, rounds) {
+  return wins/rounds * 100;
+};
