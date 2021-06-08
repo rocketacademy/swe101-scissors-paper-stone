9cf0ba9623853758ca923ac657e3b6c422b04fb4
felisatirta <felisatirta@gmail.com>
2021-06-08 19:06:42 +0800

    Project 1 SPS Part 2 base

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// PROJECT 1: Scissors Paper Stone - Part 1
+// PROJECT 1: Scissors Paper Stone - Part 1 & 2
+
+// 2 game modes: "waiting for user name" and "SPS game" (part 2)
+var currentGameMode = "waiting for user name";
+var userName = " ";
+// win-loss record (part 2)
+var playCount = 0;
+var userWinCount = 0;
+var computerWinCount = 0;
+var drawCount = 0;
 
 // create a function to generate Scissors Paper Stone
 var generateSPS = function () {
@@ -17,68 +26,125 @@ var generateSPS = function () {
   return randomItem;
 };
 
-var main = function (input) {
-  var myOutputValue = "";
+var playSPSGame = function (userName, userGuess) {
+  var message = " ";
 
-  // input validation: first check if input is a string
-  if (isNaN(Number(input)) == false) {
-    myOutputValue = "Sorry, please enter a word.";
-    return myOutputValue;
+  // input validation: first check if input is a string (part 1 - more comfortable)
+  if (isNaN(Number(userGuess)) == false) {
+    message = `Sorry ${userName}, please enter a word.`;
+    return message;
   }
 
-  // input validation: then check if input is scissors, paper, stone, reversed scissors, reversed paper or reversed stone.
-  var itemName = generateSPS();
+  // input validation: then check if input is scissors, paper, stone, reversed scissors, reversed paper or reversed stone (part 1 - more comfortable)
   if (
-    input !== "scissors" &&
-    input !== "paper" &&
-    input !== "stone" &&
-    input !== "reversed scissors" &&
-    input !== "reversed paper" &&
-    input !== "reversed stone"
+    userGuess !== "scissors" &&
+    userGuess !== "paper" &&
+    userGuess !== "stone" &&
+    userGuess !== "reversed scissors" &&
+    userGuess !== "reversed paper" &&
+    userGuess !== "reversed stone"
   ) {
-    myOutputValue = `Sorry, there are only 6 input options available: "scissors", "paper", "stone", "reversed scissors", "reversed paper" or "reversed stone". <br> <br> Please use lowercase letters only and try again.`;
-    return myOutputValue;
+    message = `Sorry ${userName}, there are only 6 input options available: "scissors", "paper", "stone", "reversed scissors", "reversed paper" or "reversed stone". <br> <br> Please use lowercase letters only and try again.`;
+    return message;
   }
 
-  //Basic Scissors Paper Stone
+  // SPS game logic
+  // Basic Scissors Paper Stone
   // 1. scissors beats paper
   // 2. paper beats stone
   // 3. stone beats scissors
-  var randomItem = generateSPS();
-  var message = `Now you can type "scissors", "paper", "stone", "reversed scissors", reversed paper" or "reversed stone" to play another round!`;
+  var itemName = generateSPS();
+  var message2 = `Now you can type "scissors", "paper", "stone", "reversed scissors", reversed paper" or "reversed stone" to play another round!`;
   if (
-    (input == "scissors" && itemName == "paper") ||
-    (input == "paper" && itemName == "stone") ||
-    (input == "stone" && itemName == "scissors")
+    (userGuess == "scissors" && itemName == "paper") ||
+    (userGuess == "paper" && itemName == "stone") ||
+    (userGuess == "stone" && itemName == "scissors")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> ${message}`;
-    return myOutputValue;
+    playCount = playCount + 1;
+    userWinCount = userWinCount + 1;
+    var winningRate = (userWinCount / playCount) * 100;
+    if (winningRate >= 50) {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You win! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're doing well, ${userName}! <br> <br> ${message2}`;
+    } else {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You win! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're not doing so well, ${userName}!  <br> <br> ${message2}`;
+    }
+    return message;
   }
 
-  // Reverse Scissors Paper Stone where the rules are reversed:
+  // (part 1) Reverse Scissors Paper Stone where the rules are reversed:
   // 1. scissors beats stone
   // 2. stone beats paper
   // 3. paper beats scissors
   if (
-    (input == "reversed scissors" && itemName == "stone") ||
-    (input == "reversed stone" && itemName == "paper") ||
-    (input == "reversed paper" && itemName == "scissors")
+    (userGuess == "reversed scissors" && itemName == "stone") ||
+    (userGuess == "reversed stone" && itemName == "paper") ||
+    (userGuess == "reversed paper" && itemName == "scissors")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> ${message}`;
-    return myOutputValue;
+    playCount = playCount + 1;
+    userWinCount = userWinCount + 1;
+    winningRate = (userWinCount / playCount) * 100;
+    if (winningRate >= 50) {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You win! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're doing well, ${userName}! <br> <br> ${message2}`;
+    } else {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You win! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're not doing so well, ${userName}!  <br> <br> ${message2}`;
+    }
+    return message;
   }
 
   // 4. if both parties choose the same object, it's a draw.
   if (
-    itemName == input ||
-    (itemName == "scissors" && input == "reversed scissors") ||
-    (itemName == "paper" && input == "reversed paper") ||
-    (itemName == "stone" && input == "reversed stone")
+    itemName == userGuess ||
+    (itemName == "scissors" && userGuess == "reversed scissors") ||
+    (itemName == "paper" && userGuess == "reversed paper") ||
+    (itemName == "stone" && userGuess == "reversed stone")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> It's a draw! <br> <br> ${message}`;
+    playCount = playCount + 1;
+    drawCount = drawCount + 1;
+    winningRate = (userWinCount / playCount) * 100;
+    if (winningRate >= 50) {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> It's a draw! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're doing well, ${userName}! <br> <br> ${message2}`;
+    } else {
+      message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> It's a draw! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're not doing so well, ${userName}!  <br> <br> ${message2}`;
+    }
+    return message;
+  }
+
+  playCount = playCount + 1;
+  computerWinCount = computerWinCount + 1;
+  winningRate = (userWinCount / playCount) * 100;
+  if (winningRate >= 50) {
+    message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You lose! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're doing well, ${userName}! <br> <br> ${message2}`;
+  } else {
+    message = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> You lose! <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}% <br> You're not doing so well, ${userName}!  <br> <br> ${message2}`;
+  }
+  return message;
+};
+
+var main = function (input) {
+  var myOutputValue = " ";
+  // collect user's name as the first input after the page loads
+  if (currentGameMode == "waiting for user name") {
+    userName = input;
+    // now that we have the user's name
+    currentGameMode = "SPS game";
+    myOutputValue = `Hello, ${userName}! Now you can type in "scissors", "paper", "stone" (or "reversed scissors", "reversed paper", "reversed stone") to start playing the SPS game!`;
+  } else if (currentGameMode == "SPS game") {
+    myOutputValue = playSPSGame(userName, input);
     return myOutputValue;
   }
 
-  myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> ${message}`;
+  // if (currentGameMode == "waiting for user name" && userName == " ") {
+  //   myOutputValue = "Please enter your name.";
+  //   return myOutputValue;
+  // }
+
   return myOutputValue;
+};
+
+var getTitleText = function () {
+  if (currentGameMode == "waiting for user name") {
+    return "What is your name?";
+  } else {
+    return "What are you feeling like? Scissors, Paper or Stone?";
+  }
 };
