144848b8b4196f6186bafd77d72a337ee9064d4c
felisatirta <felisatirta@gmail.com>
2021-06-09 01:42:10 +0800

    reverse game mode from more comfortable section

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // PROJECT 1: Scissors Paper Stone - Part 1 & 2
 
-// 2 game modes: "waiting for user name" and "SPS game" (part 2)
 var currentGameMode = "waiting for user name";
 var userName = " ";
-// win-loss record (part 2)
 var playCount = 0;
 var userWinCount = 0;
 var computerWinCount = 0;
 var drawCount = 0;
+var reverseMode = false;
 
 // create a function to generate Scissors Paper Stone
 var generateSPS = function () {
@@ -40,59 +39,78 @@ var playSPSGame = function (userName, userGuess) {
     userGuess !== "scissors" &&
     userGuess !== "paper" &&
     userGuess !== "stone" &&
-    userGuess !== "reversed scissors" &&
-    userGuess !== "reversed paper" &&
-    userGuess !== "reversed stone"
+    userGuess !== "reverse" &&
+    userGuess !== "normal"
   ) {
-    message = `Sorry ${userName}, there are only 6 input options available: "scissors", "paper", "stone", "reversed scissors", "reversed paper" or "reversed stone". <br> <br> Please use lowercase letters only and try again.`;
+    message = `Sorry ${userName}, there are only 3 input options available: "scissors", "paper", "stone". <br> <br> Please use lowercase letters only and try again.`;
     return message;
   }
 
-  // SPS game logic
-  // Basic Scissors Paper Stone
-  // 1. scissors beats paper
-  // 2. paper beats stone
-  // 3. stone beats scissors
-  // Reverse Scissors Paper Stone where the rules are reversed:
-  // 1. scissors beats stone
-  // 2. stone beats paper
-  // 3. paper beats scissors
+  if (userGuess == "reverse") {
+    reverseMode = true;
+    return "Reverse mode is on! Now, please enter 'scissors', 'paper', or 'stone'";
+  }
+
+  if (userGuess == "normal") {
+    reverseMode = false;
+    return "Game is back to normal mode! Now, please enter 'scissors', 'paper', or 'stone'.";
+  }
+
   var itemName = generateSPS();
-  var endingMessage = `Now you can type "scissors", "paper", "stone", "reversed scissors", reversed paper" or "reversed stone" to play another round!`;
-  if (
-    (userGuess == "scissors" && itemName == "paper") ||
-    (userGuess == "paper" && itemName == "stone") ||
-    (userGuess == "stone" && itemName == "scissors") ||
-    (userGuess == "reversed scissors" && itemName == "stone") ||
-    (userGuess == "reversed stone" && itemName == "paper") ||
-    (userGuess == "reversed paper" && itemName == "scissors")
-  ) {
-    playCount = playCount + 1;
-    userWinCount = userWinCount + 1;
-    var winningRate = (userWinCount / playCount) * 100;
-    var outcomeMessage = `You win!`;
+  var endingMessage = `Now you can type "scissors", "paper", "stone" to play another round! <br> <br> (To return to normal mode, please type "normal".)`;
+  // SPS game logic (normal)
+  if (currentGameMode == "SPS game" && !reverseMode) {
+    if (
+      (userGuess == "scissors" && itemName == "paper") ||
+      (userGuess == "paper" && itemName == "stone") ||
+      (userGuess == "stone" && itemName == "scissors")
+    ) {
+      playCount = playCount + 1;
+      userWinCount = userWinCount + 1;
+      var winningRate = (userWinCount / playCount) * 100;
+      var outcomeMessage = `You win!`;
+    } // if both parties choose the same object, it's a draw
+    else if (itemName == userGuess) {
+      playCount = playCount + 1;
+      drawCount = drawCount + 1;
+      winningRate = (userWinCount / playCount) * 100;
+      outcomeMessage = `It's a draw!`;
+    } // if the above two statements are false then the user has lost
+    else {
+      playCount = playCount + 1;
+      computerWinCount = computerWinCount + 1;
+      winningRate = (userWinCount / playCount) * 100;
+      outcomeMessage = `You lose!`;
+    }
   }
 
-  // 4. if both parties choose the same object, it's a draw.
-  else if (
-    itemName == userGuess ||
-    (itemName == "scissors" && userGuess == "reversed scissors") ||
-    (itemName == "paper" && userGuess == "reversed paper") ||
-    (itemName == "stone" && userGuess == "reversed stone")
-  ) {
-    playCount = playCount + 1;
-    drawCount = drawCount + 1;
-    winningRate = (userWinCount / playCount) * 100;
-    outcomeMessage = `It's a draw!`;
+  // SPS game logic (reverse)
+
+  if (currentGameMode == "SPS game" && reverseMode) {
+    if (
+      (userGuess == "scissors" && itemName == "stone") ||
+      (userGuess == "stone" && itemName == "paper") ||
+      (userGuess == "paper" && itemName == "scissors")
+    ) {
+      playCount = playCount + 1;
+      userWinCount = userWinCount + 1;
+      var winningRate = (userWinCount / playCount) * 100;
+      var outcomeMessage = `You win!`;
+    } // if both parties choose the same object, it's a draw.
+    else if (itemName == userGuess) {
+      playCount = playCount + 1;
+      drawCount = drawCount + 1;
+      winningRate = (userWinCount / playCount) * 100;
+      outcomeMessage = `It's a draw!`;
+    } // if the above two statements are false then the user has lost
+    else {
+      playCount = playCount + 1;
+      computerWinCount = computerWinCount + 1;
+      winningRate = (userWinCount / playCount) * 100;
+      outcomeMessage = `You lose!`;
+    }
   }
 
-  // if the above two statements are false then the user has lost
-  else {
-    playCount = playCount + 1;
-    computerWinCount = computerWinCount + 1;
-    winningRate = (userWinCount / playCount) * 100;
-    outcomeMessage = `You lose!`;
-  }
   var beginningMessage = `${userName}, the computer chose ${itemName}.<br> You chose ${userGuess}. <br> <br> ${outcomeMessage} <br> <br> Games played: ${playCount} <br> Games won: ${userWinCount} <br> Games lost: ${computerWinCount} <br> Number of draws: ${drawCount} <br> Win rate: ${winningRate}%.`;
 
   if (winningRate >= 50) {
@@ -100,6 +118,7 @@ var playSPSGame = function (userName, userGuess) {
   } else {
     message = `${beginningMessage} <br> You're not doing so well, ${userName}! <br> <br> ${endingMessage}`;
   }
+
   return message;
 };
 
@@ -110,18 +129,9 @@ var main = function (input) {
     userName = input;
     // now that we have the user's name
     currentGameMode = "SPS game";
-    myOutputValue = `Hello, ${userName}! Now you can type in "scissors", "paper", "stone" (or "reversed scissors", "reversed paper", "reversed stone") to start playing the SPS game!`;
-  } else if (currentGameMode == "SPS game") {
+    return `Hello, ${userName}! Now you can choose to play normal SPS game by typing in "scissors", "paper", or "stone" immediately, or you can choose to play the reverse SPS game by typing in "reverse" first.`;
+  } else {
     myOutputValue = playSPSGame(userName, input);
-    return myOutputValue;
   }
   return myOutputValue;
-};
-
-var getTitleText = function () {
-  if (currentGameMode == "waiting for user name") {
-    return "What is your name?";
-  } else {
-    return "What are you feeling like? Scissors, Paper or Stone?";
-  }
 };
