26cfbe98a329011931a8b4792feb6b05e05923c5
tong-huat <tonghuat86@gmail.com>
2021-06-08 10:25:49 +0800

    SPS reverse and normal mode git status

---
+++ script.js
@@ -0,0 +1,144 @@
+var currentGameMode = `waiting for user name`;
+var userName = ``;
+var totalTries = 0;
+var winRecord = 0;
+var drawRecord = 0;
+var SCISSORS = `scissors`;
+var PAPER = `paper`;
+var STONE = `stone`;
+var test = 0;
+var defaultIntroMessage = `Scissors<br>. <br>. <br>.<br> Paper<br>. <br>. <br>.<br> Stone <br>. <br>. <br>.<br> `;
+var defaultClosingMessage = `Pls input: scissors, paper or stone for next round<br><br><br> `;
+// creating random numbers from 1 to 3 for tagging to SPS
+var sPS = function () {
+  // generate random num from 0 to 2.99
+  var randomDecimal = Math.random() * 3;
+  // round down random decimal to integer and plus 1 for 1 to 3
+
+  var diceNum = Math.floor(randomDecimal) + 1;
+
+  // allocating a number to each SPS and showing the object as output
+  if (diceNum == 1) {
+    return SCISSORS;
+  }
+
+  if (diceNum == 2) {
+    return PAPER;
+  }
+
+  if (diceNum == 3) {
+    return STONE;
+  }
+};
+// refactor SPS
+var playSPS = function (userName, input) {
+  var myOutputValue = ``;
+  var computerChoice = sPS();
+  // validation to inform user can only input SPS
+  if (!(input == SCISSORS || input == PAPER || input == STONE))
+    return `Pls enter scissors, paper or stone!`;
+
+  totalTries = totalTries + 1;
+
+  // defining win conditions for usual SPS
+  if (
+    (input == SCISSORS && computerChoice == PAPER) ||
+    (input == PAPER && computerChoice == STONE) ||
+    (input == STONE && computerChoice == SCISSORS)
+  ) {
+    winRecord = winRecord += 1;
+    return `${defaultIntroMessage} Congrats ${userName}, you won!<br> <br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice} <br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord} draws so far<br><br><br>Cheers! <br><br> ${defaultClosingMessage}`;
+  }
+
+  // defining lose conditions for usual SPS
+  if (
+    (input == SCISSORS && computerChoice == STONE) ||
+    (input == PAPER && computerChoice == SCISSORS) ||
+    (input == STONE && computerChoice == PAPER)
+  ) {
+    return `${defaultIntroMessage} Oops ${userName}, you lost! <br> <br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice}<br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord} draws so far<br><br><br> Better luck next time!<br><br> ${defaultClosingMessage}`;
+  }
+
+  // defining draw condition for usual SPS
+  if (input == computerChoice) {
+    drawRecord = drawRecord += 1;
+    return `${defaultIntroMessage}It's a DRAW, ${userName}! <br><br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice} <br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord} draws so far<br><br><br>Pls try again <br><br> ${defaultClosingMessage}`;
+  }
+
+  return myOutputValue;
+};
+
+// refactor reversed SPS
+var playReversedSPS = function (userName, input) {
+  var myOutputValue = ``;
+  var computerChoice = sPS();
+  // validation to inform user can only input SPS
+  if (!(input == SCISSORS || input == PAPER || input == STONE))
+    return `Pls enter scissors, paper or stone!`;
+
+  totalTries = totalTries + 1;
+
+  // defining lose conditions for revered SPS
+  if (
+    (input == SCISSORS && computerChoice == PAPER) ||
+    (input == PAPER && computerChoice == STONE) ||
+    (input == STONE && computerChoice == SCISSORS)
+  ) {
+    return `X<br>${defaultIntroMessage}<br>X<br> Oops ${userName}, you lost! <br> <br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice}<br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord} draws so far<br><br><br> Its different this time! HEHEHE <br><br> ${defaultClosingMessage}`;
+  }
+
+  // defining win conditions for reversed SPS
+  if (
+    (input == SCISSORS && computerChoice == STONE) ||
+    (input == PAPER && computerChoice == SCISSORS) ||
+    (input == STONE && computerChoice == PAPER)
+  ) {
+    winRecord = winRecord += 1;
+    return `X<br>${defaultIntroMessage}<br>X<br>Congrats ${userName}, you won!<br> <br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice} <br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord} draws so far<br><br><br>You got lucky! Its different this time! <br><br> ${defaultClosingMessage} `;
+  }
+
+  // defining draw condition for reversed SPS
+  if (
+    (input == SCISSORS && computerChoice == SCISSORS) ||
+    (input == PAPER && computerChoice == PAPER) ||
+    (input == STONE && computerChoice == STONE)
+  ) {
+    drawRecord = drawRecord += 1;
+    return `X<br>${defaultIntroMessage}<br>X<br>It's a DRAW, ${userName}! <br><br> Your choice: ${input} <br> vs <br> Computer choice: ${computerChoice} <br> You won ${winRecord}/${totalTries} turns and you have ${drawRecord}  draws so far<br><br><br>Pls try again <br><br> ${defaultClosingMessage}`;
+  }
+
+  return myOutputValue;
+};
+
+var main = function (input) {
+  var myOutputValue = ``;
+
+  // set initial stage for user to input name
+  if (currentGameMode == `waiting for user name`) {
+    // set user name
+    userName = input;
+    currentGameMode = `SPS`;
+    return `Hello ${userName}! Good luck!`;
+  }
+  if (currentGameMode == `SPS`) {
+    myOutputValue = playSPS(userName, input);
+  }
+
+  if (input == `normal`) {
+    totalTries = 0;
+    winRecord = 0;
+    drawRecord = 0;
+    currentGameMode = `SPS`;
+  }
+
+  if (input == `reverse`) {
+    totalTries = 0;
+    winRecord = 0;
+    drawRecord = 0;
+    currentGameMode = `reversed SPS`;
+  }
+  if (currentGameMode == `reversed SPS`) {
+    myOutputValue = playReversedSPS(userName, input);
+  }
+  return myOutputValue;
+};
