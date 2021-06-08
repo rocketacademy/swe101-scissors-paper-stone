6f929e81cf277239274d8a16cfeb2c94ad0027e3
felisatirta <felisatirta@gmail.com>
2021-06-08 17:17:12 +0800

    refactor code, modify input validation

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
 // PROJECT 1: Scissors Paper Stone - Part 1
-// The user inputs one of "scissors", "paper", or "stone"
-// The program internally randomly chooses scissors, paper, or stone, and the program outputs whether the user won, the program won, or it's a draw.
-// Rules: scissors beats paper, paper beats stone, and stone beats scissors. If both parties choose the same object, it's a draw.
 
-var randomNumber = function () {
+// create a function to generate Scissors Paper Stone
+var generateSPS = function () {
   // produces a decimal between 0 and 2
   var randomDecimal = Math.random() * 3;
-
   // take off the decimal to get a number 0 - 2 ... add 1
   var randomInteger = Math.floor(randomDecimal) + 1;
-
-  return randomInteger;
+  // assign 1, 2, and 3 to "scissors", "paper", and "stone" respectively.
+  if (randomInteger == 1) {
+    var randomItem = "scissors";
+  } else if (randomInteger == 2) {
+    randomItem = "paper";
+  } else if (randomInteger == 3) {
+    randomItem = "stone";
+  }
+  return randomItem;
 };
 
 var main = function (input) {
-  var itemNumber = randomNumber();
+  var myOutputValue = "";
 
-  // assigning 1, 2, and 3 to "scissors", "paper", and "stone" respectively.
-  if (itemNumber == 1) {
-    var itemName = "scissors";
+  // input validation: first check if input is a string
+  if (isNaN(Number(input)) == false) {
+    myOutputValue = "Sorry, please enter a word.";
+    return myOutputValue;
   }
 
-  if (itemNumber == 2) {
-    itemName = "paper";
+  // input validation: then check if input is scissors, paper, stone, reversed scissors, reversed paper or reversed stone.
+  var itemName = generateSPS();
+  if (
+    input !== "scissors" &&
+    input !== "paper" &&
+    input !== "stone" &&
+    input !== "reversed scissors" &&
+    input !== "reversed paper" &&
+    input !== "reversed stone"
+  ) {
+    myOutputValue = `Sorry, there are only 6 input options available: "scissors", "paper", "stone", "reversed scissors", "reversed paper" or "reversed stone". <br> <br> Please use lowercase letters only and try again.`;
+    return myOutputValue;
   }
-
-  if (itemNumber == 3) {
-    itemName = "stone";
-  }
-
-  // the default output is user loses. Using "More Comfortable" Formatting.
-  var myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
 
   //Basic Scissors Paper Stone
   // 1. scissors beats paper
   // 2. paper beats stone
   // 3. stone beats scissors
+  var randomItem = generateSPS();
+  var message = `Now you can type "scissors", "paper", "stone", "reversed scissors", reversed paper" or "reversed stone" to play another round!`;
   if (
     (input == "scissors" && itemName == "paper") ||
     (input == "paper" && itemName == "stone") ||
     (input == "stone" && itemName == "scissors")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
+    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> ${message}`;
+    return myOutputValue;
   }
 
   // Reverse Scissors Paper Stone where the rules are reversed:
@@ -53,7 +64,8 @@ var main = function (input) {
     (input == "reversed stone" && itemName == "paper") ||
     (input == "reversed paper" && itemName == "scissors")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
+    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You win! <br> <br> ${message}`;
+    return myOutputValue;
   }
 
   // 4. if both parties choose the same object, it's a draw.
@@ -63,23 +75,10 @@ var main = function (input) {
     (itemName == "paper" && input == "reversed paper") ||
     (itemName == "stone" && input == "reversed stone")
   ) {
-    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> It's a draw! <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
+    myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> It's a draw! <br> <br> ${message}`;
+    return myOutputValue;
   }
 
-  // Input Validation
-  // Sometimes the user types bad and types something other than "scissors", "paper", or "stone" during gameplay. Add input validation to kindly let the user know that there are only 3 input options, and ask them to try again.
-  // Also add input validation for the reverse game
-  // The user can choose to try their luck at reversed mode by adding the word "reversed" to their choice. For example: "reversed stone".
-  if (
-    input !== "scissors" &&
-    input !== "paper" &&
-    input !== "stone" &&
-    input !== "reversed scissors" &&
-    input !== "reversed paper" &&
-    input !== "reversed stone"
-  ) {
-    myOutputValue = `There are only 3 input options available: scissors, paper or stone. <br> <br> Please use lowercase letters only and try again.`;
-  }
-
+  myOutputValue = `The computer chose ${itemName}.<br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> ${message}`;
   return myOutputValue;
 };
