d078875f949211b02f33623453ea298663626250
yapjunyi <yapjunyi95@gmail.com>
2021-06-09 01:42:43 +0800

    Normal, Reversed, Korean Complete

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 var userWins = 0;
 var computerWins = 0;
-var userName = " ";
-var userEntry = 0;
+var userName = "";
 var mode = "";
-
 var main = function (input) {
-  if (input != "" && userEntry == 0) {
+  // Check if player enter username
+  if (input != "" && userName == "") {
     userName = input;
-    userEntry += 1;
-    return `Your username is ${userName}.`;
+    return `Your username is ${userName}. Pls key in normal or reversed or korean for game mode.`;
   }
-  if (input != "" && userEntry == 1) {
+  // Normal mode selection
+  if (input == "normal") {
     mode = input;
-    userEntry += 1;
+    return `You have selected ${mode} mode.`;
+  } // Reversed mode selection
+  if (input == "reversed") {
+    mode = input;
     return `You have selected ${mode} mode.`;
   }
-  if (mode == "normal" && userEntry == 2) {
+  // Korean mode selection
+  if (input == "korean") {
+    mode = input;
+    return `You have selected ${mode} mode.`;
+  }
+
+  //Normal mode game play
+  if (mode == "normal") {
     var getNumber = sortNum();
     var returnType = ScissorsPaperStone(getNumber);
     console.log(returnType);
     var returnInput = input.toLowerCase();
-    var myOutputValue = outcome(returnType, returnInput);
+    var myOutputValue = sps(returnType, returnInput);
     return myOutputValue;
-  }
-  if (mode == "reversed" && userEntry == 2) {
+  } // Reverse mode game play
+  if (mode == "reversed") {
     var getNumber = sortNum();
     var returnType = ScissorsPaperStone(getNumber);
     var returnInput = input.toLowerCase();
-    var myOutputValue = reversedOutcome(returnType, returnInput);
+    var myOutputValue = reversedSps(returnType, returnInput);
+    return myOutputValue;
+  } // Korean mode game play
+  if (mode == "korean") {
+    var getNumber = sortNum();
+    var returnType = ScissorsPaperStone(getNumber);
+    var returnInput = input.toLowerCase();
+    var myOutputValue = koreanSps(returnType, returnInput);
+    return myOutputValue;
   } else return `You have not entered your username.`;
 };
-
 var sortNum = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal);
   var randomNumber = randomInteger + 1;
-
   return randomNumber;
 };
-
 var ScissorsPaperStone = function (translateNumber) {
   if (translateNumber == 1) {
     return "scissors";
@@ -47,9 +61,13 @@ var ScissorsPaperStone = function (translateNumber) {
     return "paper";
   } else return "stone";
 };
-
-var outcome = function (type, input) {
+var sps = function (type, input) {
   var totalTimes = 0;
+  //Enter scissors paper stone
+  if (!(input == "scissors" || input == "paper" || input == "stone")) {
+    return `Please Enter scissors, paper or stone`;
+  }
+  //Gameplay
   if (
     (input == "scissors" && type == "paper") ||
     (input == "paper" && type == "stone") ||
@@ -58,9 +76,9 @@ var outcome = function (type, input) {
     userWins += 1;
     totalTimes = userWins + computerWins;
     return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You win! <br> <br>
-   So far${userName}, You've win ${userWins}/${totalTimes} turns. Good on you! 
+   So far ${userName}, You've won ${userWins}/${totalTimes} turns. Good on you! 
    
-   > Now you can type "scissors" "paper" or "stone" to play another round!`;
+  <br><br>  Now you can type "scissors" "paper" or "stone" to play another round or change game mode!`;
   }
   if (input == type) {
     console.log("Capture");
@@ -69,11 +87,15 @@ var outcome = function (type, input) {
     computerWins += 1;
     totalTimes = userWins + computerWins;
     return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> 
-     So far ${userName}, you've been winning ${computerWins}/${totalTimes} turns. Pretty good!`;
+     So far ${userName}, you've been winning ${userWins}/${totalTimes} turns. Pretty good!`;
   }
 };
-var reversedOutcome = function (type, input) {
+var reversedSps = function (type, input) {
   var totalTimes = 0;
+  //Enter scissors paper stone
+  if (!(input == "scissors" || input == "paper" || input == "stone")) {
+    return `Please Enter scissors, paper or stone`;
+  }
   if (
     (input == "paper" && type == "scissors") ||
     (input == "stone" && type == "paper") ||
@@ -86,16 +108,47 @@ var reversedOutcome = function (type, input) {
     return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You win! <br> <br>
    So far${userName}, You've win ${userWins}/${totalTimes} turns. Good on you! 
    
-   > Now you can type "scissors" "paper" or "stone" to play another round!`;
+   <br> Now you can type "scissors" "paper" or "stone" to play another round or change!`;
   }
   if (input == type) {
     console.log("Capture");
-
     return `The computer chose ${type}. <br> You chose ${input}. <br> <br> It's a draw ${userName}! Try again. <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
   } else {
     computerWins += 1;
     totalTimes = userWins + computerWins;
     return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> 
-     So far ${userName}, you've been winning ${computerWins}/${totalTimes} turns. Pretty good!`;
+     So far ${userName}, you've been winning ${userWins}/${totalTimes} turns. Pretty good!`;
+  }
+};
+
+var koreanSps = function (type, input) {
+  var totalTimes = 0;
+  //Enter scissors paper stone
+  if (!(input == "scissors" || input == "paper" || input == "stone")) {
+    return `Please Enter scissors, paper or stone`;
+  }
+  //Gameplay
+  if (
+    (input == "scissors" && type == "paper") ||
+    (input == "paper" && type == "stone") ||
+    (input == "stone" && type == "scissors")
+  ) {
+    userWins += 1;
+    totalTimes = userWins + computerWins;
+    return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You win! <br> <br>
+   So far ${userName}, You've won ${userWins}/${totalTimes} turns. Good on you! 
+   
+  <br><br>  Now you can type "scissors" "paper" or "stone" to play another round or change game mode!`;
+  }
+  if (input == type) {
+    if (userWins > computerWins) {
+      return `The computer chose ${type}. <br> You chose ${input}. <br> <br> It's a draw ${userName}! The game has ended.. <br> <br> You won.<br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
+    } else
+      return `The computer chose ${type}. <br> You chose ${input}. <br> <br> It's a draw ${userName}! The game has ended.. <br> <br> You lost. <br> <br> Now you can type "scissors" "paper" or "stone" to play another round!`;
+  } else {
+    computerWins += 1;
+    totalTimes = userWins + computerWins;
+    return `The computer chose ${type}. <br> You chose ${input}. <br> <br> You lose! Bummer. <br> <br> 
+     So far ${userName}, you've been winning ${userWins}/${totalTimes} turns. Pretty good!`;
   }
 };
