df46441cf38f912b2703b04d3be89c406a7ac4e0
zhang-yilinn <zhangyilin54@gmail.com>
2021-06-08 14:43:18 +0800

    final project finish

--- script.js
+++ script.js
@@ -1,53 +1,154 @@
+//trouble shoot reversed os message
+var totalMatches = 0;
+var timesPlayerWon = 0;
+var numberOfDraws = 0;
+var userName = ``;
+var message;
+var normal = `normal`;
+var korean = `korean`;
+var reversed = `reversed`;
+var gameMode = normal;
+var player = `player`;
+var currentWinner = `${userName}`;
+var myOutputValue;
 var main = function (input) {
+  var helpChoose = help();
   var elementfinal = elements();
-  myOutputValue = `The computer chose ${elementfinal} .
+  if (input == ``) {
+    input = helpChoose;
+  }
+  if (input == normal || input == korean || input == reversed) {
+    gameMode = input;
+    myOutputValue = `Your gamemode has been set to ${gameMode}. Please type 'scissors', 'paper' or 'stone' to play the game`;
+  }
+  customKorean = function (customPhrase) {
+    Message = `Thank you for playing ${userName}! The computer chose ${elementfinal} .
 You chose ${input}.
-
-You lose! Bummer.
-
-Now you can type "scissors" "paper" or "stone" to play another round!`;
+<br>${customPhrase}.
+<br> Type in something other than "scissors", "paper","stone" ,"normal","reversed" or "korean" to set/change your username.
+<br>Now you can type "scissors" "paper" or "stone" to play another round!
+<br>You can also type in "normal", "reversed" or "korean" to change your game mode.(Your current game mode is ${gameMode})
+<br> If you would like the computer to play for you, leave the input box blank`;
+    return Message;
+  };
+  console.log(gameMode);
+  console.log(customKorean(`${gameMode}`));
+  customMessage = function (customPhrase) {
+    Message = `Thank you for playing ${userName}! The computer chose ${elementfinal} .
+You chose ${input}.
+<br>${customPhrase}.
+<br>You've won ${timesPlayerWon}/${totalMatches} matches with ${numberOfDraws} draws.
+<br> Type in something other than "scissors", "paper","stone" ,"normal","reversed" or "korean" to set/change your username.
+<br>Now you can type "scissors" "paper" or "stone" to play another round!
+<br>You can also type in "normal", "reversed" or "korean" to change your game mode.(Your current game mode is ${gameMode})
+<br> If you would like the computer to play for you , leave the input box blank`;
+    return Message;
+  };
+  //for username
   if (
     input != "scissors" &&
     input != "paper" &&
     input != "stone" &&
-    input != "reversed stone" &&
-    input != "reversed paper" &&
-    input != "reversed scissors"
+    input != "normal" &&
+    input != "reversed" &&
+    input != "korean"
   ) {
-    myOutputValue = `You've entered an invalid option. Please type 'scissors'
-    , 'paper', 'stone', 'reveresed paper', 'reversed stone' or 'reversed scissors' to try again`;
+    userName = input;
+    myOutputValue = `Your username has been set to ${userName}. <br>Please type 'normal', 'korean' or 'reversed' to play the game(the gamemode has been set to normal by default) or type 'scissors', 'paper' or stone' to start the game`;
   }
-  if (
-    (input == `scissors` && elementfinal == "scissors ✂️") ||
-    (input == `paper` && elementfinal == "paper 📃") ||
-    (input == `stone` && elementfinal == "stone 💎") ||
-    (input == `reversed scissors` && elementfinal == "scissors ✂️") ||
-    (input == `reversed paper` && elementfinal == "paper 📃") ||
-    (input == `reversed stone` && elementfinal == "stone 💎")
-  ) {
-    myOutputValue = `The computer chose ${elementfinal} .
-You chose ${input}.
-
-It's a draw! Bummer.
-
-Now you can type "scissors" "paper" or "stone" to play another round!`;
+  if (input == `computer`) {
+    myOutputValue = `Your username has been set to computer and the computer will now play on your behalf. <br>Please type 'normal', 'korean' or 'reversed' to play the game(the gamemode has been set to normal by default)`;
   }
-  if (
-    (input == `scissors` && elementfinal == "paper 📃") ||
-    (input == `paper` && elementfinal == "stone 💎") ||
-    (input == `stone` && elementfinal == "scissors ✂️") ||
-    (input == `reversed scissors` && elementfinal == "stone 💎") ||
-    (input == `reversed paper` && elementfinal == "scissors ✂️") ||
-    (input == `reversed stone` && elementfinal == "paper 📃")
-  ) {
-    myOutputValue = `The computer chose ${elementfinal} .
-You chose ${input}.
-
-You win! Hooray.
-
-Now you can type "scissors" "paper" or "stone" to play another round!`;
+  //for normal gamemode
+  if (gameMode == normal) {
+    if (
+      (input == `scissors` && elementfinal == "stone 💎") ||
+      (input == `paper` && elementfinal == "scissors ✂️") ||
+      (input == `stone` && elementfinal == "paper 📃")
+    ) {
+      totalMatches = totalMatches + 1;
+      myOutputValue = customMessage(`You lose! Bummer`);
+    }
+    if (
+      (input == `scissors` && elementfinal == "scissors ✂️") ||
+      (input == `paper` && elementfinal == "paper 📃") ||
+      (input == `stone` && elementfinal == "stone 💎")
+    ) {
+      totalMatches = totalMatches + 1;
+      numberOfDraws = numberOfDraws + 1;
+      myOutputValue = customMessage(`It's a draw! Bummer`);
+    }
+    if (
+      (input == `scissors` && elementfinal == "paper 📃") ||
+      (input == `paper` && elementfinal == "stone 💎") ||
+      (input == `stone` && elementfinal == "scissors ✂️")
+    ) {
+      totalMatches = totalMatches + 1;
+      timesPlayerWon = timesPlayerWon + 1;
+      myOutputValue = customMessage(`You win! Hooray`);
+    }
   }
-
+  //for reversed gamemode
+  if (gameMode == `reversed`) {
+    if (
+      (input == `scissors` && elementfinal == "paper 📃") ||
+      (input == `paper` && elementfinal == "stone 💎") ||
+      (input == `stone` && elementfinal == "scissors ✂️")
+    ) {
+      totalMatches = totalMatches + 1;
+      myOutputValue = customMessage(`You lose! Bummer`);
+    }
+    if (
+      (input == `scissors` && elementfinal == "scissors ✂️") ||
+      (input == `paper` && elementfinal == "paper 📃") ||
+      (input == `stone` && elementfinal == "stone 💎")
+    ) {
+      totalMatches = totalMatches + 1;
+      numberOfDraws = numberOfDraws + 1;
+      myOutputValue = customMessage(`It's a draw! Bummer`);
+    }
+    if (
+      (input == `scissors` && elementfinal == "stone 💎") ||
+      (input == `paper` && elementfinal == "scissors ✂️") ||
+      (input == `stone` && elementfinal == "paper 📃")
+    ) {
+      totalMatches = totalMatches + 1;
+      timesPlayerWon = timesPlayerWon + 1;
+      myOutputValue = customMessage(`You win! Hooray`);
+    }
+  }
+  //for korean gamemode
+  if (gameMode == korean) {
+    if (
+      (input == `scissors` && elementfinal == "stone 💎") ||
+      (input == `paper` && elementfinal == "scissors ✂️") ||
+      (input == `stone` && elementfinal == "paper 📃")
+    ) {
+      currentWinner = `computer`;
+      myOutputValue = customKorean(
+        `The most recent winner is ${currentWinner} muk-jji-ppa!`
+      );
+    }
+    if (
+      (input == `scissors` && elementfinal == "scissors ✂️") ||
+      (input == `paper` && elementfinal == "paper 📃") ||
+      (input == `stone` && elementfinal == "stone 💎")
+    ) {
+      myOutputValue = customKorean(
+        `Its a draw! The most recent winner is ${currentWinner}. ${currentWinner} won! muk-jji-ppa!`
+      );
+    }
+    if (
+      (input == `scissors` && elementfinal == "paper 📃") ||
+      (input == `paper` && elementfinal == "stone 💎") ||
+      (input == `stone` && elementfinal == "scissors ✂️")
+    ) {
+      currentWinner = `${userName}`;
+      myOutputValue = customKorean(
+        `The most recent winner is ${currentWinner} muk-jji-ppa!`
+      );
+    }
+  }
   return myOutputValue;
 };
 var elements = function () {
@@ -63,4 +164,18 @@ var elements = function () {
     var element = "stone 💎";
   }
   return element;
+};
+var help = function () {
+  var Decimal = Math.random() * 3 + 1;
+  var number = Math.floor(Decimal);
+  if (number == 1) {
+    var helpelement = "scissors";
+  }
+  if (number == 2) {
+    var helpelement = "paper";
+  }
+  if (number == 3) {
+    var helpelement = "stone";
+  }
+  return helpelement;
 };
