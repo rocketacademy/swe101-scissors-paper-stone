636373bb12c6e0820aa002e1ef3165266864080b
iameleannn <ngelean@gmail.com>
2021-06-10 22:11:56 +0800

    submission

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -3,15 +3,22 @@ Please fill out the survey before submitting the pull request. Thanks!
 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 
 How many hours did you spend on this assignment?
+2 hours
 
 Please fill in one error and/or error message you received while working on this assignment.
+Unable to compile due to missing "}"
 
 What part of the assignment did you spend the most time on?
+Trying to fix the bug due to case sensitive.
 
 Comfort Level (1-5):
+3
 
 Completeness Level (1-5):
+3
 
 What did you think of this deliverable?
+Its fun!
 
 Is there anything in this code that you feel pleased about?
+I finally managed to write my first code !
--- script.js
+++ script.js
@@ -1,4 +1,103 @@
 var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+  var output = randomSPS();
+  console.log("Random Guess");
+  console.log(output);
+  var myOutputValue =
+    "You lost! You guessed: " + input + "Random Guess " + output;
+
+  // If both parties choose the same object, it's a draw.
+  if (input == output) {
+    return "Tie! Try again!";
+  }
+
+  // if scissors beats paper, paper beats stone, and stone beats scissors.
+
+  if (
+    (input == "Scissors" && output == "Paper") ||
+    (input == "Paper" && output == "Stone") ||
+    (input == "Stone" && output == "Scissors")
+  ) {
+    return "You Win!" + "The random guess is " + output + "You chose " + input;
+  }
+
+  // if paper lose scissors, stone lose paper, scissors lose stone.
+
+  if (
+    (input == "Paper" && output == "Scissors") ||
+    (input == "Stone" && output == "Paper") ||
+    (input == "Scissors" && output == "Stone")
+  ) {
+    return (
+      "The random guess is " +
+      output +
+      "You lose as you choose " +
+      input +
+      ". Try again"
+    );
+  }
+
+  if (
+    input != "Scissors" &&
+    input != "Paper" &&
+    input != "Stone" &&
+    input != "Reversed Scissors" &&
+    input != "Reversed Paper" &&
+    input != "Reversed Stone"
+  ) {
+    return "Please only put Scissors, Paper, or Stone.";
+  }
+
+  // inverse game
+
+  //Reversed Scissors lose paper, Reversed Paper lose Stone, Reversed Stone lose Scissors
+
+  if (
+    (input == "Reversed Scissors" && output == "Paper") ||
+    (input == "Reversed Paper" && output == "Stone") ||
+    (input == "Reversed Stone" && output == "Scissors")
+  ) {
+    return (
+      "The random guess is " +
+      output +
+      " You lose as you choose " +
+      input +
+      ". Try again"
+    );
+  }
+
+  // Reversed Scissors win Stone, Reversed Paper win Scissors, Reversed Stone win Paper
+
+  if (
+    (input == "Reversed Scissors" && output == "Stone") ||
+    (input == "Reversed Paper" && output == "Scissors") ||
+    (input == "Reversed Stone" && output == "Paper")
+  ) {
+    return "You Win!" + "The random guess is " + output + "You chose " + input;
+  }
+
+  // If tie
+
+  if (
+    (input == "Reversed Scissors" && output == "Scissors") ||
+    (input == "Reversed Paper" && output == "Paper") ||
+    (input == "Reversed Stone" && output == "Stone")
+  ) {
+    return "Tie! Try again!";
+  }
+
+  return output;
+};
+
+var randomSPS = function () {
+  var randomGuess = Math.random() * 3;
+  var randomGuess = Math.floor(randomGuess);
+  if (randomGuess == 0) {
+    return "Scissors";
+  }
+  if (randomGuess == 1) {
+    return "Paper";
+  }
+  if (randomGuess == 2) {
+    return "Stone";
+  }
 };
