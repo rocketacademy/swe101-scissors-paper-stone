67e2b10f3a0bc939c0ee6402d20a22c208fdcf74
helloeddychan <eddy@newcampus.co>
2021-06-11 21:46:24 +0800

    SPS project 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//first game is user name
+var currentGameMode = "waiting for user name";
+var userName = "";
+
+//track record of player and computer wins
+var playerWin = 0;
+var computerWin = 0;
+
 //generate a random number to be assigned to Rock, Paper or Scissors
 var generateRandomInterger = function (max) {
   var randomDecimal = Math.random() * max;
@@ -21,37 +29,64 @@ var generateSPS = function () {
 
 //accept input and compare against generated number to check who wins
 var main = function (input) {
-  var computerSPS = generateSPS();
-  console.log(computerSPS);
-  var genericOutput =
-    "The computer chose " +
-    computerSPS +
-    "." +
-    "<br>" +
-    "You chose " +
-    input +
-    "." +
-    "<br>";
-  var myOutputValue = genericOutput + "You lose! Bummer";
-  //draw conditions
-  if (input == computerSPS) {
-    myOutputValue = genericOutput + "It's a draw!";
-  }
-  //winning conditions
-  if (input == "Rock" && computerSPS == "Scissors") {
-    myOutputValue = genericOutput + "You win!";
-  }
-  if (input == "Paper" && computerSPS == "Rock") {
-    myOutputValue = genericOutput + "You win!";
-  }
-  if (input == "Scissors" && computerSPS == "Paper") {
-    myOutputValue = genericOutput + "You win!";
-  }
+  if (currentGameMode == "waiting for user name") {
+    userName = input;
+    currentGameMode = "SPS";
+    myOutputValue = "Hello " + userName;
+  } else if (currentGameMode == "SPS") {
+    var computerSPS = generateSPS();
+    console.log(computerSPS);
+    var genericOutput =
+      "The computer chose " +
+      computerSPS +
+      ".<br> You chose " +
+      input +
+      ".<br>";
+    var myOutputValue = genericOutput;
+    //draw conditions
+    if (input == computerSPS) {
+      myOutputValue = genericOutput + "It's a draw!";
+    }
+    //winning conditions
+    if (input == "Rock" && computerSPS == "Scissors") {
+      playerWin += 1;
+      myOutputValue = genericOutput + "You win!";
+    }
+    if (input == "Paper" && computerSPS == "Rock") {
+      playerWin += 1;
+      myOutputValue = genericOutput + "You win!";
+    }
+    if (input == "Scissors" && computerSPS == "Paper") {
+      playerWin += 1;
+      myOutputValue = genericOutput + "You win!";
+    }
 
-  return (
-    myOutputValue +
-    "<br>Now you can type 'Scissors' 'paper' or 'stone' to play another round!"
-  );
+    //losing conditions
+    if (input == "Rock" && computerSPS == "Paper") {
+      computerWin += 1;
+      myOutputValue = genericOutput + "You lose, bummer!";
+    }
+    if (input == "Paper" && computerSPS == "Scissors") {
+      computerWin += 1;
+      myOutputValue = genericOutput + "You lose, bummer!";
+    }
+    if (input == "Scissors" && computerSPS == "Rock") {
+      computerWin += 1;
+      myOutputValue = genericOutput + "You lose, bummer!";
+    }
+    return (
+      myOutputValue +
+      "<br><br>Now you can type 'Scissors' 'Paper' or 'Stone' to play another round!<br>" +
+      userName +
+      ", you won " +
+      playerWin +
+      " times." +
+      "<br> The computer won " +
+      computerWin +
+      " times"
+    );
+  }
+  return myOutputValue;
 };
 //reject answers that are not Rock, Paper, Scissors
 //var acceptableEntry = function (accept) {
