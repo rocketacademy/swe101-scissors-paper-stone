8248179208349f4cd60365d9b75f2d28808b03db
gcmillora <gcmillora@up.edu.ph>
2021-06-22 00:49:47 +0800

    added win and games count

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var winCount = 0;
+var gameCount = 0;
+var draw = 0;
+
+var gamemode = "input";
+
 var getRandomChoice = function () {
   var randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice == 0) return "paper";
@@ -10,23 +16,28 @@ var validateInput = function (input) {
   else return 0;
 };
 var getWinner = function (playerChoice) {
+  gameCount += 1;
   var computerChoice = getRandomChoice();
   var outputMessage = "";
   if (playerChoice == "rock" && computerChoice == "scissors") {
-    outputMessage = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    outputMessage = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    winCount += 1;
   } else if (playerChoice == "paper" && computerChoice == "rock") {
-    outputMessage = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    outputMessage = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    winCount += 1;
   } else if (playerChoice == "scissors" && computerChoice == "paper") {
-    outputMessage = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    outputMessage = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    winCount += 1;
   } else if (playerChoice == computerChoice) {
     outputMessage = `It's a draw! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    winCount += 1;
   } else {
-    outputMessage = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    outputMessage = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`;
   }
-  outputMessage +=
-    "<br><br>Reversed! Haha, try playing again by typing 'scissors', 'rock', or 'paper'. Good luck!";
+  outputMessage += `<br><br>You have won ${winCount} out of ${gameCount} games.<br>Try playing again by typing 'scissors', 'rock', or 'paper'. Good luck!`;
   return outputMessage;
 };
+
 var doPaperRockScissors = function (input) {
   if (validateInput(input)) return getWinner(input);
   else return "Please input either paper, rock, scissors only. Try again :)";
