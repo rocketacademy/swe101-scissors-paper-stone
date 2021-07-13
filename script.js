ddcd4bc25a490d1ba46674fb378ea5a7e3af3758
Kimmi Lee <lee.kimmixq@gmail.com>
2021-07-14 02:34:44 +0900

    completed basic version

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+var HAND_SCISSORS = "scissors";
+var HAND_PAPER = "paper";
+var HAND_STONE = "stone";
+
+// GENERATE RANDOM INTEGER (0-2) FOR COMPUTER
+var randComp = function () {
+  var randNum = Math.random() * 3;
+  var randInt = Math.floor(randNum);
+  return randInt;
+};
+
 var main = function (input) {
-  var myOutputValue = "hello world";
+  var myOutputValue = "you lose!";
+  var compHand = randComp();
+  console.log(compHand);
+  // ASSIGN INPUT TO INTEGER (0 SCI, 1 PAP, 2 STO)
+  var playerHand;
+  if (input == HAND_SCISSORS) {
+    playerHand = 0;
+  }
+  if (input == HAND_PAPER) {
+    playerHand = 1;
+  }
+  if (input == HAND_STONE) {
+    playerHand = 2;
+  }
+  // COMPARE PLAYER AND COMPUTER HANDS (WIN)
+  if (playerHand - compHand == -1 || playerHand - compHand == 2) {
+    myOutputValue = "you win!";
+  }
+  // COMPARE PLAYER AND COMPUTER HANDS (DRAW)
+  if (playerHand == compHand) {
+    myOutputValue = "it's a draw!";
+  }
   return myOutputValue;
 };
